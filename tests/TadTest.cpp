cdb65ae8b7c29cf112658e7f25679cd434435697
Adam Gibson <adam@skymind.io>
2017-01-07 11:43:57 +0900

    get rid of array.h, start to add tests

--- tests/TadTest.cpp
+++ tests/TadTest.cpp
@@ -4,21 +4,58 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-
 #include <shape.h>
 
 
 namespace {
-    class TADTest : public testing::Test {
+    class VectorTest : public testing::Test {
 
+    };
+
+    class ShapeTest :  public testing::Test {
+    public:
+        int vectorShape[2] = {1,2};
     };
 }
 
+::testing::AssertionResult arrsEquals(int n,int *assertion,int *other) {
+    for(int i = 0; i < n; i++) {
+        if(assertion[i] != other[i])
+            return ::testing::AssertionFailure()  << i << "  is not equal";
 
+    }
+    return ::testing::AssertionSuccess();
 
-TEST_F(TADTest,test1) {
-    int shapeTest[2] = {2,2};
-    int *shapeBuffer = shape::shapeBuffer(2,shapeTest);
-    ASSERT_EQ("","");
-    delete[] shapeBuffer;
 }
+
+TEST_F(VectorTest,VectorTadShape) {
+    int rowVector[2] = {2,2};
+    int *rowBuffer = shape::shapeBuffer(2,rowVector);
+    int rowDimension = 1;
+
+    int columnVector[2] = {2,2};
+    int *colShapeBuffer = shape::shapeBuffer(2,columnVector);
+    int colDimension = 0;
+
+
+    shape::TAD *rowTad = new shape::TAD(rowBuffer,&rowDimension,1);
+    int *rowTadShapeBuffer = rowTad->shapeInfoOnlyShapeAndStride();
+    int *rowTadShape = shape::shapeOf(rowTadShapeBuffer);
+    shape::TAD *colTad = new shape::TAD(colShapeBuffer,&colDimension,1);
+    int *colTadShapeBuffer = colTad->shapeInfoOnlyShapeAndStride();
+    int *colTadShape = shape::shapeOf(colTadShapeBuffer);
+    int assertionShape[2] = {1,5};
+    EXPECT_TRUE(arrsEquals(2,assertionShape,rowTadShape));
+    EXPECT_TRUE(arrsEquals(2,assertionShape,colTadShape));
+
+    delete[] rowBuffer;
+    delete[] colShapeBuffer;
+    delete rowTad;
+    delete colTad;
+}
+
+
+TEST_F(ShapeTest,IsVector) {
+    ASSERT_TRUE(shape::isVector(vectorShape,2));
+}
+
