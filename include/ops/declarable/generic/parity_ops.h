b10b215c32cec03c0c3cc4ed0553bbb79976962d
raver119@gmail.com <raver119@gmail.com>
2017-09-16 14:37:22 +0300

    irrelevant: shape fix for testcustom shape fn

--- include/ops/declarable/generic/parity_ops.h
+++ include/ops/declarable/generic/parity_ops.h
@@ -595,14 +595,19 @@ namespace nd4j {
         }
         DECLARE_SHAPE_FN(testcustom) {
             // this test op will just return back original shape doubled
+            int *shapeOf;
+            ALLOCATE(shapeOf, block.getWorkspace(), shape::rank(inputShape), int);
+
             int *newShape;
             ALLOCATE(newShape, block.getWorkspace(), shape::shapeInfoLength(inputShape), int);
-            memcpy(newShape, inputShape, shape::shapeInfoByteLength(inputShape));
 
-            // THIS IS WRONG SHAPEINFO
-            // but we don't care at this moment - it's just simple test
             for (int e = 0; e < shape::rank(inputShape); e++)
-                newShape[e+1] *= 2;
+                shapeOf[e] = inputShape[e+1] * 2;
+
+
+            shape::shapeBuffer(shape::rank(inputShape), shapeOf, newShape);
+
+            RELEASE(shapeOf, block.getWorkspace());
 
             return newShape;
         }
