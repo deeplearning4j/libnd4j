4d9c09d4f530882f47a278a63c668480810f0df9
raver119 <raver119@gmail.com>
2017-09-22 18:47:29 +0300

    tad test :/

--- tests/layers_tests/TadTests.h
+++ tests/layers_tests/TadTests.h
@@ -19,7 +19,27 @@ public:
 TEST_F(TadTests, Test4DTad1) {
     std::unique_ptr<NDArray<float>> arraySource(NDArrayFactory::linspace(1.0f, 10000.0f, 10000));
 
+    std::unique_ptr<NDArray<float>> arrayExp(new NDArray<float>('c', {2, 1, 4, 4}));
+    std::unique_ptr<NDArray<float>> arrayBad(new NDArray<float>('c', {2, 1, 4, 4}));
 
+    arrayExp->setBuffer(arraySource->getBuffer() + 50);
+    arrayExp->printShapeInfo("Exp shapeBuffer: ");
+
+
+    std::vector<int> badShape({4, 2, 1, 4, 4, 80, 16, 4, 1, 0, -1, 99});
+
+    arrayBad->setBuffer(arraySource->getBuffer() + 50);
+    arrayBad->setShapeInfo(badShape.data());
+    arrayBad->printShapeInfo("Bad shapeBuffer: ");
+
+    std::unique_ptr<NDArray<float>> sumExp(arrayExp->sum({1}));
+    std::unique_ptr<NDArray<float>> sumBad(arrayBad->sum({1}));
+
+
+    sumExp->printBuffer("Proper result:");
+    sumBad->printBuffer("Wrong  result:");
+
+    ASSERT_TRUE(sumBad->equalsTo(sumExp.get()));
 }
 
 #endif //LIBND4J_TADTESTS_H
