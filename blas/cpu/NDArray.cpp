8794d89060bfb1950fc7bcd0cb76fb064d4e8250
Yurii <yurii@skymind.io>
2017-09-27 18:00:06 +0300

    fix bug in addrowvector method

--- blas/cpu/NDArray.cpp
+++ blas/cpu/NDArray.cpp
@@ -880,7 +880,7 @@ T& NDArray<T>::operator()(const int i, const int j) {
 //////////////////////////////////////////////////////////////////////////
 template<typename T>
     void NDArray<T>::addRowVector(const NDArray<T> *row, NDArray<T>* target) {
-        if (rankOf() != 2 || target->rankOf() != 2 || rows() != target->rows() || columns() != target->columns() || !row->isRowVector() || columns() != rows->columns())
+        if (rankOf() != 2 || target->rankOf() != 2 || rows() != target->rows() || columns() != target->columns() || !row->isRowVector() || columns() != row->columns())
             throw std::invalid_argument("NDArray::addRowVector: wrong arguments !");
 
         int dimension[1] = {1};
@@ -897,7 +897,7 @@ template<typename T>
 // This method adds given row to all rows in this NDArray, this array becomes affected
     template<typename T>
     void NDArray<T>::addiRowVector(const NDArray<T> *row) {
-    if (rankOf() != 2 !row->isRowVector() || columns() != rows->columns())
+    if (rankOf() != 2 || !row->isRowVector() || columns() != row->columns())
         throw std::invalid_argument("NDArray::addiRowVector: wrong arguments !");
 
         int dimension[1] = {1};
@@ -915,7 +915,7 @@ template<typename T>
 // This method adds given column to all columns in this NDArray, this array becomes affected
     template<typename T>
     void NDArray<T>::addiColumnVector(const NDArray<T> *column) {
-        if (rankOf() != 2 !row->isColumnVector() || rows() != column->rows())
+        if (rankOf() != 2 || !column->isColumnVector() || rows() != column->rows())
             throw std::invalid_argument("NDArray::addiColumnVector: wrong arguments !");
 
         int dimension[1] = {0};
@@ -934,7 +934,7 @@ template<typename T>
 // This method multiplies each column of this array by given argument-column, this array becomes affected
     template<typename T>
     void NDArray<T>::muliColumnVector(const NDArray<T> *column) {
-        if (rankOf() != 2 !row->isColumnVector() || rows() != column->rows())
+        if (rankOf() != 2 || !column->isColumnVector() || rows() != column->rows())
             throw std::invalid_argument("NDArray::muliColumnVector: wrong arguments !");
 
         int dimension[1] = {0};
