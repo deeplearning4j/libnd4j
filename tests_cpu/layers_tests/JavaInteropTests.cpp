fc6084cbeb06345d2790734988523670a8d0eba3
raver119@gmail.com <raver119@gmail.com>
2017-10-09 11:27:30 +0300

    NativeOps calculateOutputShape

--- tests_cpu/layers_tests/JavaInteropTests.cpp
+++ tests_cpu/layers_tests/JavaInteropTests.cpp
@@ -4,6 +4,7 @@
 
 #include <NativeOps.h>
 #include <NDArray.h>
+#include <ops/declarable/CustomOperations.h>
 #include "testlayers.h"
 
 using namespace nd4j;
@@ -15,5 +16,28 @@ public:
 
 
 TEST_F(JavaInteropTests, TestShapeExposure1) {
+    NDArray<float> input('c', {1, 2, 5, 4});
+    NDArray<float> weights('c', {3, 2, 2, 2});
+    NDArray<float> exp('c', {1, 3, 5, 4});
 
+
+    NativeOps nativeOps;
+
+    nd4j::ops::conv2d<float> op;
+
+    std::vector<float> tArgs({});
+    std::vector<int> iArgs({2, 2, 1, 1, 0, 0, 1, 1, 1});
+
+
+    Nd4jPointer ptrs[] = {(Nd4jPointer) input.getShapeInfo(), (Nd4jPointer) weights.getShapeInfo()};
+
+    auto shapeList = nativeOps.calculateOutputShapesFloat(nullptr, op.getOpHash(), ptrs, 2, tArgs.data(), tArgs.size(), iArgs.data(), iArgs.size());
+
+    ASSERT_EQ(1, shapeList->size());
+
+    ASSERT_EQ(exp.rankOf(), shape::rank(shapeList->at(0)));
+    ASSERT_EQ(exp.sizeAt(0), shape::shapeOf(shapeList->at(0))[0]);
+    ASSERT_EQ(exp.sizeAt(1), shape::shapeOf(shapeList->at(0))[1]);
+    ASSERT_EQ(exp.sizeAt(2), shape::shapeOf(shapeList->at(0))[2]);
+    ASSERT_EQ(exp.sizeAt(3), shape::shapeOf(shapeList->at(0))[3]);
 }
