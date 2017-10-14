ebd29e75f3c7035d77eb631db5fdbfcaf319c3bf
raver119@gmail.com <raver119@gmail.com>
2017-10-14 13:53:27 +0300

    scope :/

---
+++ include/graph/Scope.h
@@ -0,0 +1,22 @@
+//
+// Created by raver119 on 14.10.2017.
+//
+
+#ifndef LIBND4J_SCOPE_H
+#define LIBND4J_SCOPE_H
+
+namespace nd4j {
+    namespace graph {
+        template <typename T>
+        class Scope {
+        protected:
+        public:
+            // attach GiG here, with shared namespace?
+            // or just rebuilt graph leaf?
+            //          ¯\_(ツ)_/¯
+        };
+    }
+}
+
+
+#endif //LIBND4J_SCOPE_H
---
+++ include/graph/impl/Scope.cpp
@@ -0,0 +1,5 @@
+//
+// Created by raver119 on 14.10.2017.
+//
+
+#include "Scope.h"
