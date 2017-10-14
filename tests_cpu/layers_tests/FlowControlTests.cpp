281d6c8783522e782e1b2dc2301ea8cf85c7ae9d
raver119@gmail.com <raver119@gmail.com>
2017-10-14 14:02:19 +0300

    few comments

--- tests_cpu/layers_tests/FlowControlTests.cpp
+++ tests_cpu/layers_tests/FlowControlTests.cpp
@@ -31,18 +31,27 @@ TEST_F(FlowControlTests, SwitchTest1) {
     variableSpace->putVariable(-2, condtionX);
     variableSpace->putVariable(-3, condtionY);
 
+    // this is just 2 ops, that are executed sequentially. We don't really care bout them
     auto nodeA = new Node<float>(OpType_TRANSFORM, 0, 1, {-1}, {2});
     auto nodeB = new Node<float>(OpType_TRANSFORM, 0, 2, {1}, {3});
+
+    // this is our condition op, we'll be using Equals condition, on variables conditionX and conditionY (ids -2 and -3 respectively)
     auto nodeCondition = new Node<float>(OpType_BOOLEAN, 0, 119, {-2, -3});
 
+    // we're creating this op manually in tests, as always.
     nd4j::ops::eq_scalar<float> eqOp;
     nodeCondition->setCustomOp(&eqOp);
 
+    // now, this is Switch operation. It takes BooleanOperation operation in,
+    // and based on evaluation result (true/false) - it'll pass data via :0 or :1 output
+    // other idx will be considered disabled, and that graph branch won't be executed
     auto nodeSwitch = new Node<float>(OpType_CUSTOM, 0, 3, {2, 119}, {4, 5});
 
     nd4j::ops::Switch<float> switchOp;
     nodeSwitch->setCustomOp(&switchOp);
 
+
+    // these 2 ops are connected to FALSE and TRUE outputs. output :0 considered FALSE, and output :1 considered TRUE
     auto nodeZ0 = new Node<float>(OpType_TRANSFORM, 0, 4, {}, {});
     nodeZ0->pickInput(3, 0);
     auto nodeZ1 = new Node<float>(OpType_TRANSFORM, 35, 5, {}, {});
@@ -58,9 +67,11 @@ TEST_F(FlowControlTests, SwitchTest1) {
 
     graph.buildGraph();
 
+    // we're making sure nodes connected to the Switch have no other inputs in this Graph
     ASSERT_EQ(1, nodeZ0->input()->size());
     ASSERT_EQ(1, nodeZ1->input()->size());
 
+    // just validating topo sort
     ASSERT_EQ(0, nodeA->getLayer());
     ASSERT_EQ(0, nodeCondition->getLayer());
     ASSERT_EQ(1, nodeB->getLayer());
@@ -68,20 +79,25 @@ TEST_F(FlowControlTests, SwitchTest1) {
     ASSERT_EQ(3, nodeZ0->getLayer());
     ASSERT_EQ(3, nodeZ1->getLayer());
 
-
+    // executing graph
     Nd4jStatus status = GraphExecutioner<float>::execute(&graph);
 
     ASSERT_EQ(ND4J_STATUS_OK, status);
 
+    // we know that Switch got TRUE evaluation, so :0 should be inactive
     ASSERT_FALSE(nodeZ0->isActive());
+
+    // and :1 should be active
     ASSERT_TRUE(nodeZ1->isActive());
 
     std::pair<int,int> unexpected(4,0);
     std::pair<int,int> expectedResultIndex(5,0);
     ASSERT_TRUE(variableSpace->hasVariable(expectedResultIndex));
 
+    // getting output of nodeZ1
     auto output = variableSpace->getVariable(expectedResultIndex)->getNDArray();
 
+    // and veryfing it against known expected value
     ASSERT_NEAR(-118.0f, output->getScalar(0), 1e-5f);
 }
 
