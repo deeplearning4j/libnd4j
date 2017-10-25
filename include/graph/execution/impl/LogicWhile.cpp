7007e9aeda96f0226970c23a39d364ba0716ab72
raver119@gmail.com <raver119@gmail.com>
2017-10-25 22:30:33 +0300

    more validation in while

--- include/graph/execution/impl/LogicWhile.cpp
+++ include/graph/execution/impl/LogicWhile.cpp
@@ -12,10 +12,18 @@ namespace nd4j {
         Nd4jStatus LogicWhile<T>::processNode(Graph<T> *graph, Node<T> *node) {
             auto __variableSpace = graph->getVariableSpace();
 
-            int scopeConditionIndex = node->input()->at(0).first;
-            int scopeBodyIndex = node->input()->at(1).first;
+            // total number of inputs. 2 last inputs are scopes
+            int inputs = node->input()->size();
 
-            nd4j_debug("While [%i] got [%i] inputs\n", node->id(), node->input()->size());
+            if (inputs < 3) {
+                nd4j_printf("While [%i]: loop should have at least 1 external variable announced\n", node->id());
+                return ND4J_STATUS_BAD_INPUT;
+            }
+
+            int scopeConditionIndex = node->input()->at(inputs - 2).first;
+            int scopeBodyIndex = node->input()->at(inputs - 1).first;
+
+            nd4j_debug("While [%i]: got [%i] inputs\n", node->id(), node->input()->size());
 
             // we're running condition nodes now
             auto scope = graph->scopeById(scopeConditionIndex);
@@ -23,11 +31,16 @@ namespace nd4j {
             while (true && breaker < 10000000) {
                 int lastNode = 0;
                 // we're running condition scope first
-                nd4j_debug("While [%i] got [%i] ops in condition scope [%i]\n", node->id(), scope->nodes()->size(), scopeConditionIndex);
+                nd4j_debug("While [%i]: got [%i] ops in condition scope [%i]\n", node->id(), scope->nodes()->size(), scopeConditionIndex);
 
                 for (auto v: *scope->nodes()) {
                     GraphExecutioner<T>::executeFlatNode(graph, v, __variableSpace);
                     lastNode = v->id();
+                }
+
+                if (!__variableSpace->hasVariable(lastNode)) {
+                    nd4j_printf("While [%i]: got no results out of conditional loop\n", node->id());
+                    return ND4J_STATUS_KERNEL_FAILURE;
                 }
 
                 // now we should take result of the Scope run, and evaluate it
