693a009e9c6683fc4e413f35d810f0aeb62c3d50
raver119 <raver119@gmail.com>
2017-11-18 16:14:12 +0200

    - crelu test - concat now allows negative axis

--- tests_cpu/layers_tests/DeclarableOpsTests2.cpp
+++ tests_cpu/layers_tests/DeclarableOpsTests2.cpp
@@ -148,4 +148,22 @@ TEST_F(DeclarableOpsTests2, Test_FloorMod_1) {
     delete result;
 }
 
+TEST_F(DeclarableOpsTests2, Test_CRelu_1) {
+    NDArray<float> x('c', {2, 2}, {1.0, 2.0, 3.0, 4.0});
+    NDArray<float> exp('c', {2, 4}, {1.0, 2.0, 0, 0, 3.0, 4.0, 0, 0});
+
+    nd4j::ops::crelu<float> op;
+
+    auto result = op.execute({&x}, {}, {});
+
+    ASSERT_EQ(ND4J_STATUS_OK, result->status());
+
+    auto z = result->at(0);
+
+    ASSERT_TRUE(exp.isSameShape(z));
+    ASSERT_TRUE(exp.equalsTo(z));
+
+    delete result;
+}
+
  
