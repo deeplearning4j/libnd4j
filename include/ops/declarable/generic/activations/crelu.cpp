b64af1fa8cf7b4b544556fa1277c56de3b2fb34f
raver119@gmail.com <raver119@gmail.com>
2017-11-21 13:06:00 +0300

    draft :/

--- include/ops/declarable/generic/activations/crelu.cpp
+++ include/ops/declarable/generic/activations/crelu.cpp
@@ -43,5 +43,40 @@ namespace nd4j {
 
             return new ShapeList(newShape);
         }
+
+
+        CUSTOM_OP_IMPL(crelu_bp, 2, 1, false, 0, 0) {
+            auto input = INPUT_VARIABLE(0);
+            auto epsilonNext = INPUT_VARIABLE(0);
+            auto epsilon = OUTPUT_VARIABLE(0);
+
+            // at first step we build fwd activation
+            nd4j::ops::crelu<T> op;
+            auto tmpResult = op.execute({input}, {}, {}); 
+            auto actv = tmpResult->at(0);
+
+            // now we do RELU backward pass
+            auto lambda = LAMBDA_TT(_x, _e) {
+                return _x > (T) 0.0f ? _e  : (T) 0.0f;
+            };
+            actv->applyPairwiseLambda(epsilon, lambda);
+
+            // now we split updated array into 2 chunks along last dimension
+
+
+            // and now we subtract two parts of epsilons and pass result out
+
+            delete tmpResult;
+            return ND4J_STATUS_OK;
+        }
+
+        DECLARE_SHAPE_FN(crelu_bp) {
+            auto inShape = inputShape->at(0);
+            int* newShape;
+            ALLOCATE(newShape, block.getWorkspace(), shape::shapeInfoLength(inShape), int);
+            memcpy(newShape, inShape, shape::shapeInfoByteLength(inShape));
+
+            return new ShapeList(newShape);
+        }
     }
 }
