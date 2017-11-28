57a7b6584031f04d5cc5adf5939f2ba2d8b02f74
raver119 <raver119@gmail.com>
2017-11-28 19:03:50 +0300

    few tests transferred

--- tests_cpu/layers_tests/PlaygroundTests.cpp
+++ tests_cpu/layers_tests/PlaygroundTests.cpp
@@ -17,8 +17,9 @@ public:
 };
 
 
+
 TEST_F(PlaygroundTests, LambdaTest_1) {
-    NDArray<float> array('c', {128, 512});
+    NDArray<float> array('c', {8192, 1024});
     NDArrayFactory<float>::linspace(1, array);
 
     auto lambda = LAMBDA_F(_x) {
@@ -32,18 +33,104 @@ TEST_F(PlaygroundTests, LambdaTest_1) {
     }
 
 
-    /*
-#pragma omp parallel for schedule(guided)
-    for (int i = 0; i < numIterations; i++) {
-        for (int e = 0; e < array.lengthOf(); e++) {
-            array.buffer()[e] = array.buffer()[e] + 32.12f;
-        }
+    auto timeEnd = std::chrono::system_clock::now();
+
+    auto outerTime = std::chrono::duration_cast<std::chrono::microseconds> (timeEnd - timeStart).count();
+
+    nd4j_printf("Lambda 1 time %lld us\n", outerTime / numIterations);
+}
+
+
+
+
+TEST_F(PlaygroundTests, LambdaTest_2) {
+    NDArray<float> array('c', {8192, 1024});
+    NDArray<float> row('c', {1, 1024});
+    NDArrayFactory<float>::linspace(1, array);
+
+    auto lambda = LAMBDA_F(_x) {
+        return _x + 32.12f;
+    };
+
+    auto timeStart = std::chrono::system_clock::now();
+
+    for (int e = 0; e < numIterations; e++) {
+        array.template applyBroadcast<simdOps::Add<float>>({1}, &row);
     }
-    */
+
 
     auto timeEnd = std::chrono::system_clock::now();
 
     auto outerTime = std::chrono::duration_cast<std::chrono::microseconds> (timeEnd - timeStart).count();
 
-    nd4j_printf("Time %lld us\n", outerTime / numIterations);
+    nd4j_printf("Broadcast time %lld us\n", outerTime / numIterations);
+}
+
+
+TEST_F(PlaygroundTests, NoCacheTest_1) {
+    std::vector<NDArray<float> *> pool(100);
+    NDArray<float> source('c', {8192, 1024});
+    for (int e = 0; e < pool.size(); e++)
+        pool[e] = source.dup();
+
+    auto lambda = LAMBDA_F(_x) {
+        return _x * 32.12f;
+    };
+
+    auto timeStart = std::chrono::system_clock::now();
+    int cnt = 0;
+    for (int e = 0; e < numIterations; e++) {
+        auto v = pool[99 - (cnt++)];
+        v->applyLambda(lambda);
+
+        if (cnt == 100)
+            cnt = 0;
+    }
+
+    auto timeEnd = std::chrono::system_clock::now();
+
+    auto outerTime = std::chrono::duration_cast<std::chrono::microseconds> (timeEnd - timeStart).count();
+
+    nd4j_printf("Non-cached time %lld us\n", outerTime / numIterations);
+
+    for (auto v: pool)
+        delete v;
+}
+
+
+TEST_F(PlaygroundTests, NoCacheTest_2) {
+    std::vector<NDArray<float> *> pool1(100);
+    std::vector<NDArray<float> *> pool2(100);
+    NDArray<float> source('c', {8192, 1024});
+    for (int e = 0; e < pool1.size(); e++) {
+        pool1[e] = source.dup();
+        pool2[e] = source.dup();
+    }
+
+    auto lambda = LAMBDA_FF(_x, _y) {
+        return _x * 32.12f + _y;
+    };
+
+    auto timeStart = std::chrono::system_clock::now();
+    int cnt = 0;
+    for (int e = 0; e < numIterations; e++) {
+        auto v1 = pool1[99 - cnt];
+        auto v2 = pool2[cnt++];
+        v1->applyPairwiseLambda(v2, lambda);
+
+        if (cnt == 100)
+            cnt = 0;
+    }
+
+    auto timeEnd = std::chrono::system_clock::now();
+
+    auto outerTime = std::chrono::duration_cast<std::chrono::microseconds> (timeEnd - timeStart).count();
+
+    nd4j_printf("Non-cached PWT time %lld us\n", outerTime / numIterations);
+
+    for (auto v: pool1)
+        delete v;
+
+    for (auto v: pool2)
+        delete v;
 }
