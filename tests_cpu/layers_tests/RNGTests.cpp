cd85bfdd68a619d5cfe964232c43419a96fd1dda
raver119@gmail.com <raver119@gmail.com>
2017-12-04 13:11:24 +0300

    few rng tests

--- tests_cpu/layers_tests/RNGTests.cpp
+++ tests_cpu/layers_tests/RNGTests.cpp
@@ -37,7 +37,55 @@ public:
 };
 
 TEST_F(RNGTests, Test_Dropout_1) {
-    NDArray<float> x('c', {10, 10});
+    NDArray<float> x0('c', {10, 10});
+    NDArray<float> x1('c', {10, 10});
+    NDArray<float> nexp0('c', {10, 10});
+    NDArray<float> nexp1('c', {10, 10});
+    NDArray<float> nexp2('c', {10, 10});
 
-    NDArrayFactory<float>::linspace(1, x);
+    nexp0.assign(-1.0f);
+    nexp1.assign(-2.0f);
+    nexp2.assign(-3.0f);
+
+    NDArrayFactory<float>::linspace(1, x0);
+    NDArrayFactory<float>::linspace(1, x1);
+
+    float prob[] = {0.5f};
+
+    x0.template applyRandom<randomOps::DropOut<float>>(_rngA, nullptr, &x0, prob);
+    x1.template applyRandom<randomOps::DropOut<float>>(_rngB, nullptr, &x1, prob);
+
+    ASSERT_TRUE(x0.equalsTo(&x1));
+
+    // this check is required to ensure we're calling wrong signature
+    ASSERT_FALSE(x0.equalsTo(&nexp0));
+    ASSERT_FALSE(x0.equalsTo(&nexp1));
+    ASSERT_FALSE(x0.equalsTo(&nexp2));
+}
+
+TEST_F(RNGTests, Test_DropoutInverted_1) {
+    NDArray<float> x0('c', {10, 10});
+    NDArray<float> x1('c', {10, 10});
+    NDArray<float> nexp0('c', {10, 10});
+    NDArray<float> nexp1('c', {10, 10});
+    NDArray<float> nexp2('c', {10, 10});
+
+    nexp0.assign(-1.0f);
+    nexp1.assign(-2.0f);
+    nexp2.assign(-3.0f);
+
+    NDArrayFactory<float>::linspace(1, x0);
+    NDArrayFactory<float>::linspace(1, x1);
+
+    float prob[] = {0.5f};
+
+    x0.template applyRandom<randomOps::DropOutInverted<float>>(_rngA, nullptr, &x0, prob);
+    x1.template applyRandom<randomOps::DropOutInverted<float>>(_rngB, nullptr, &x1, prob);
+
+    ASSERT_TRUE(x0.equalsTo(&x1));
+
+    // this check is required to ensure we're calling wrong signature
+    ASSERT_FALSE(x0.equalsTo(&nexp0));
+    ASSERT_FALSE(x0.equalsTo(&nexp1));
+    ASSERT_FALSE(x0.equalsTo(&nexp2));
 }
