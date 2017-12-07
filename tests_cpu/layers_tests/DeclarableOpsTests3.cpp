1fd742f5caefafaff459d40140ba9696faa970ad
raver119@gmail.com <raver119@gmail.com>
2017-12-07 17:24:06 +0300

    Norm test

--- tests_cpu/layers_tests/DeclarableOpsTests3.cpp
+++ tests_cpu/layers_tests/DeclarableOpsTests3.cpp
@@ -89,6 +89,42 @@ TEST_F(DeclarableOpsTests3, Test_Rint_1) {
 }
 
 
+TEST_F(DeclarableOpsTests3, Test_Norm_1) {
+    NDArray<double> x('c', {100, 100});
+    NDArrayFactory<double>::linspace(1, x);
+
+    std::vector<int> dims({1});
+    nd4j::ops::norm<double> op;
+
+    auto result0 = op.execute({&x}, {0}, {1});
+
+    auto z0 = result0->at(0);
+    auto exp0 = x.template reduceAlongDims<simdOps::NormFrobenius<double>>(dims, false);
+    ASSERT_TRUE(exp0.isSameShape(z0));
+    ASSERT_TRUE(exp0.equalsTo(z0));
+
+    delete result0;
+
+    auto result1 = op.execute({&x}, {1}, {1});
+    
+    auto z1 = result1->at(0);
+    auto exp1 = x.template reduceAlongDims<simdOps::Norm2<double>>(dims, false);
+    ASSERT_TRUE(exp1.isSameShape(z1));
+    ASSERT_TRUE(exp1.equalsTo(z1));
+
+    delete result1;
+
+    auto result4 = op.execute({&x}, {4}, {1});
+    
+    auto z4 = result4->at(0);
+    auto exp4= x.template reduceAlongDims<simdOps::NormMax<double>>(dims, false);
+    ASSERT_TRUE(exp4.isSameShape(z4));
+    ASSERT_TRUE(exp4.equalsTo(z4));
+
+    delete result4;
+}
+
+
 TEST_F(DeclarableOpsTests3, Test_Range_1) {
     NDArray<float> start('c', {1, 1}, {2});
     NDArray<float> stop('c', {1, 1}, {0});
