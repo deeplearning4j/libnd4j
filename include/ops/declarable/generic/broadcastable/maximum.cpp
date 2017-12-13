ea127488df01709a024d9ee46e485ffc2f75d87e
raver119@gmail.com <raver119@gmail.com>
2017-12-13 16:56:02 +0300

    next pack

--- include/ops/declarable/generic/broadcastable/maximum.cpp
+++ include/ops/declarable/generic/broadcastable/maximum.cpp
@@ -7,6 +7,15 @@
 
 namespace nd4j {
     namespace ops {
+        /**
+         * This is one of auto-broadcastable operations. It accepts 2 operands, and operation is applied based on their shapes:
+         * 1) if shapes are equal that's pairwise operation, result will have the same shape.
+         * 2) if shape X is scalar and shape Y is array - result will have shape equal to Y.
+         * 3) if shape X is array and shape Y is scalar - result will have shape equal to X.
+         * 4) if shape X and Y are both arrays, but shapes aren't equal - result shape will be broadcast result.
+         * 
+         * This operation returns Z = Max(X, Y)
+         */
         CUSTOM_OP_IMPL(maximum, 2, 1, true, 0, 0) {
             auto x = INPUT_VARIABLE(0);
             auto y = INPUT_VARIABLE(1);
