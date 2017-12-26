e3b0a0a458e0c4c9017161f7a55bcada158407b5
raver119@gmail.com <raver119@gmail.com>
2017-12-26 19:46:13 +0300

    yet another test

--- tests_cpu/layers_tests/DeclarableOpsTests4.cpp
+++ tests_cpu/layers_tests/DeclarableOpsTests4.cpp
@@ -213,7 +213,7 @@ TEST_F(DeclarableOpsTests4, Test_StridedSlice_1) {
 
     auto z = result->at(0);
 
-    z->printIndexedBuffer("Z");
+    //z->printIndexedBuffer("Z");
 
     ASSERT_TRUE(exp.isSameShape(z));
     ASSERT_TRUE(exp.equalsTo(z));
@@ -235,7 +235,30 @@ TEST_F(DeclarableOpsTests4, Test_StridedSlice_2) {
 
     auto z = result->at(0);
 
-    z->printIndexedBuffer("Z");
+    //z->printIndexedBuffer("Z");
+
+    ASSERT_TRUE(exp.isSameShape(z));
+    ASSERT_TRUE(exp.equalsTo(z));
+
+    delete result;
+}
+
+
+TEST_F(DeclarableOpsTests4, Test_StridedSlice_3) {
+    NDArray<float> x('c', {2, 3}, {1, 2, 3, 4, 5, 6});
+    NDArray<float> a('c', {1, 2}, {1, 2});
+    NDArray<float> b('c', {1, 2}, {2, 3});
+    NDArray<float> c('c', {1, 2}, {1, 1});
+    NDArray<float> exp('c', {1, 1}, {6.0});
+
+    nd4j::ops::strided_slice<float> op;
+    auto result = op.execute({&x, &a, &b, &c}, {}, {0, 0, 0, 0, 1});
+
+    ASSERT_EQ(ND4J_STATUS_OK, result->status());
+
+    auto z = result->at(0);
+
+    //z->printIndexedBuffer("Z");
 
     ASSERT_TRUE(exp.isSameShape(z));
     ASSERT_TRUE(exp.equalsTo(z));
