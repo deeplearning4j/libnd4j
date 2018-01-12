6208231b3db2fe2095c73ddc003f5d4c93f911d3
raver119@gmail.com <raver119@gmail.com>
2018-01-12 21:17:16 +0300

    move cross to helpers

--- include/ops/declarable/generic/parity_ops/cross.cpp
+++ include/ops/declarable/generic/parity_ops/cross.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <ops/declarable/headers/parity_ops.h>
+#include <ops/declarable/helpers/cross.h>
 
 namespace nd4j {
 namespace ops {
@@ -15,58 +16,14 @@ namespace ops {
 
         // TODO: we might want to lift this restriction
         REQUIRE_TRUE(a->isSameShape(b),0, "Cross: A and B should have equal shape");
+        REQUIRE_TRUE(a->sizeAt(-1) == 3, 0, "Cross: outer dimension of A and B should be equal to 3");
 
         auto o = OUTPUT_VARIABLE(0);
 
         if (a->lengthOf() == 3) {
-            auto a0 = a->getScalar(0);
-            auto a1 = a->getScalar(1);
-            auto a2 = a->getScalar(2);
-
-            auto b0 = b->getScalar(0);
-            auto b1 = b->getScalar(1);
-            auto b2 = b->getScalar(2);
-
-            o->putScalar(0, a1 * b2 - a2 * b1);
-            o->putScalar(1, a2 * b0 - a0 * b2);
-            o->putScalar(2, a0 * b1 - a1 * b0);
+            helpers::_cross(a, b, o);
         } else {
-            REQUIRE_TRUE(a->sizeAt(-1) == 3, 0, "Cross: outer dimension of A and B should be equal to 3");
-            auto _a = a->reshape(a->ordering(), {-1, 3});
-            auto _b = b->reshape(b->ordering(), {-1, 3});
-            auto _o = o->reshape(o->ordering(), {-1, 3});
-
-            auto tadsA = NDArrayFactory<T>::allTensorsAlongDimension(_a, {1});
-            auto tadsB = NDArrayFactory<T>::allTensorsAlongDimension(_b, {1});
-            auto tadsO = NDArrayFactory<T>::allTensorsAlongDimension(_o, {1});
-
-            int tads = tadsA->size();
-
-            #pragma omp parallel for simd schedule(static)
-            for (int e = 0; e < tads; e++) {
-                auto a_ = tadsA->at(e);
-                auto b_ = tadsB->at(e);
-                auto o_ = tadsO->at(e);
-
-                auto a0 = a_->getScalar(0);
-                auto a1 = a_->getScalar(1);
-                auto a2 = a_->getScalar(2);
-
-                auto b0 = b_->getScalar(0);
-                auto b1 = b_->getScalar(1);
-                auto b2 = b_->getScalar(2);
-
-                o_->putScalar(0, a1 * b2 - a2 * b1);
-                o_->putScalar(1, a2 * b0 - a0 * b2);
-                o_->putScalar(2, a0 * b1 - a1 * b0);
-            }
-
-            delete tadsA;
-            delete tadsB;
-            delete tadsO;
-            delete _a;
-            delete _b;
-            delete _o;
+            helpers::_crossBatched(a, b, o);
         }
 
         return ND4J_STATUS_OK;
