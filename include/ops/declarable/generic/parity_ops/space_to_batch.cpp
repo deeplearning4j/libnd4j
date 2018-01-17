5e5d09393fc459ee63a146295f16af3f3283c69a
raver119@gmail.com <raver119@gmail.com>
2018-01-17 15:08:37 +0400

    tmp commit (validation)

--- include/ops/declarable/generic/parity_ops/space_to_batch.cpp
+++ include/ops/declarable/generic/parity_ops/space_to_batch.cpp
@@ -7,6 +7,13 @@
 namespace nd4j {
 namespace ops {
     CUSTOM_OP_IMPL(space_to_batch, 3, 1, false, 0, -2) {
+        auto input = INPUT_VARIABLE(0);
+        auto blocks = INPUT_VARIABLE(1);
+        auto padding = INPUT_VARIABLE(2);
+
+        REQUIRE_TRUE(input->rankOf() >= 1 + block->lengthOf() + 1, 0, "SpaceToBatch: blocks length + 2 should match input rank at least");
+        REQUIRE_TRUE(padding->rankOf() == 2, 0, "SpaceToBatch: padding should have rank of 2, but got %i instead", padding->rankOf());
+        REQUIRE_TRUE(padding->columns() == 2 && blocks->lengthOf() == padding->rows(), 0, "SpaceToBatch: padding should have M rows and 2 columns");
 
         return Status::OK();
     }
