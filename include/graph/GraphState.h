6730ab572e6c4b593b2f5f14ef049688fa537b62
raver119 <raver119@gmail.com>
2018-01-23 20:17:07 +0300

    initial commit

---
+++ include/graph/GraphState.h
@@ -0,0 +1,14 @@
+//
+// Created by raver119 on 23.01.18.
+//
+
+#ifndef LIBND4J_GRAPHSTATE_H
+#define LIBND4J_GRAPHSTATE_H
+
+
+class GraphState {
+
+};
+
+
+#endif //LIBND4J_GRAPHSTATE_H
---
+++ include/graph/impl/GraphState.cpp
@@ -0,0 +1,5 @@
+//
+// Created by raver119 on 23.01.18.
+//
+
+#include <graph/GraphState.h>
