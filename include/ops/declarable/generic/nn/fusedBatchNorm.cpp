3c7bd628aa1bee1e5b9cf277f740c5663e24a6d9
Yurii <yurii@skymind.io>
2018-01-31 00:10:24 +0200

    - providing correct behavior of reduce-like methods for new and old scalar/vector shapes - testing and refining fused_batch_norm op

--- include/ops/declarable/generic/nn/fusedBatchNorm.cpp
+++ include/ops/declarable/generic/nn/fusedBatchNorm.cpp
@@ -7,22 +7,20 @@
 namespace nd4j {
 namespace ops {
 
-CUSTOM_OP_IMPL(fused_batch_norm, 3, 1, false, 1, 2) {
+CUSTOM_OP_IMPL(fused_batch_norm, 3, 1, false, 0, 2) {
 
-    NDArray<T>* x      = INPUT_VARIABLE(0);                 // [bS,iH,iW,iD] or [bS,iD,iH,iW]
+    NDArray<T>* x      = INPUT_VARIABLE(0);                 // [bS,iH,iW,iD] (NHWC) or [bS,iD,iH,iW] (NCHW)
     NDArray<T>* scale  = INPUT_VARIABLE(1);                 // [iD]
     NDArray<T>* offset = INPUT_VARIABLE(2);                 // [iD]
 
-    NDArray<T>* y = OUTPUT_VARIABLE(0);                     
-    NDArray<T>* batchMean = OUTPUT_VARIABLE(1);
-    NDArray<T>* batchVar  = OUTPUT_VARIABLE(2);
+    NDArray<T>* y = OUTPUT_VARIABLE(0);                     // [bS,iH,iW,iD] (NHWC) or [bS,iD,iH,iW] (NCHW)
+    NDArray<T>* batchMean = OUTPUT_VARIABLE(1);             // [iD]
+    NDArray<T>* batchVar  = OUTPUT_VARIABLE(2);             // [iD]
 
-    const bool dataFormat = (bool)INT_ARG(0);  // 0->NHWC, 1->NCHW
+    const bool dataFormat = (bool)INT_ARG(0);               // 0->NHWC, 1->NCHW
     const bool isTraining = (bool)INT_ARG(1);    
 
-    REQUIRE_TRUE(x->rankOf() == 4, 0, "CUSTOM_OP fused_batch_norm: the rank of input x array must be equal to 4 !");
-    REQUIRE_TRUE(scale->rankOf() == 1, 0, "CUSTOM_OP fused_batch_norm: the rank of input scale array must be equal to 1 !");
-    REQUIRE_TRUE(offset->rankOf() == 1, 0, "CUSTOM_OP fused_batch_norm: the rank of input offset array must be equal to 1 !");    
+    REQUIRE_TRUE(x->rankOf() == 4, 0, "CUSTOM_OP fused_batch_norm: the rank of input x array must be equal to 4 !");    
 
     int bS = x->sizeAt(0);              // batch size
     int iH, iW, iD;                     // input height, input width, input depth(number of channels)        
@@ -37,59 +35,57 @@ CUSTOM_OP_IMPL(fused_batch_norm, 3, 1, false, 1, 2) {
         iD = x->sizeAt(3);   
     }    
 
-    REQUIRE_TRUE(scale->sizeAt(0)  == iD, 0, "CUSTOM_OP fused_batch_norm: the length of input scale array must be number of channels (input iD) !");
-    REQUIRE_TRUE(offset->sizeAt(0) == iD, 0, "CUSTOM_OP fused_batch_norm: the length of input offset array must be number of channels (input iD) !");
+    REQUIRE_TRUE(scale->rankOf() == 1  && scale->sizeAt(0)  == iD, 0, "CUSTOM_OP fused_batch_norm: wrong shape of input scale array !");
+    REQUIRE_TRUE(offset->rankOf() == 1 && offset->sizeAt(0) == iD, 0, "CUSTOM_OP fused_batch_norm: wrong shape of input offset array !");        
 
     NDArray<T>* mean(nullptr), *variance(nullptr);
     if(!isTraining){
         mean     = INPUT_VARIABLE(3);   
         variance = INPUT_VARIABLE(4);   
-        REQUIRE_TRUE(mean->rankOf() == 1, 0, "CUSTOM_OP fused_batch_norm: the rank of input mean array must be equal to 1 !");
-        REQUIRE_TRUE(variance->rankOf() == 1, 0, "CUSTOM_OP fused_batch_norm: the rank of input variance array must be equal to 1 !");    
-        REQUIRE_TRUE(mean->sizeAt(0)  == iD, 0, "CUSTOM_OP fused_batch_norm: the length of input scale array must be number of channels (input iD) !");
-        REQUIRE_TRUE(variance->sizeAt(0) == iD, 0, "CUSTOM_OP fused_batch_norm: the length of input variance array must be number of channels (input iD) !");
+        REQUIRE_TRUE(mean->rankOf() == 1     && mean->sizeAt(0) == iD,     0, "CUSTOM_OP fused_batch_norm: wrong shape of input mean array !");
+        REQUIRE_TRUE(variance->rankOf() == 1 && variance->sizeAt(0) == iD, 0, "CUSTOM_OP fused_batch_norm: wrong shape of input variance array !");        
     }
     else {
         REQUIRE_TRUE(block.width() == 3, 0, "CUSTOM_OP fused_batch_norm: when isTraining=true then number of input arrays must be equal to 3 !");   
         std::vector<int> shape = {iD};
         mean = new NDArray<T>(scale->ordering(), shape, block.getWorkspace());
-        variance = new NDArray<T>(scale->ordering(), shape, block.getWorkspace());
+        variance = new NDArray<T>(scale->ordering(), shape, block.getWorkspace());        
     }
 
     T epsilon;
-    if(block.getTArguments()->size() > 0)
-        epsilon = T_ARG(0);
+    if(block.getTArguments()->size() > 0)         
+        epsilon = T_ARG(0) > (T)1.001e-5 ? T_ARG(0) : (T)1.001e-5;    
     else 
         epsilon = 0.001;
-    
-    epsilon = epsilon > (T)1.001e-5 ? epsilon : (T)1.001e-5;    
     
     const int restSize = x->lengthOf() / iD;    
     NDArray<T> xAffected(x->ordering(), {restSize, iD}, block.getWorkspace());
     xAffected.assign(x);
 
     const int restSizeMinusOne = (restSize > 1) ? (restSize - 1) : 1;
-    T restSizeInv = (T)1.0 / restSize;
-    T restSizeAdjust = (T)restSize / restSizeMinusOne;
+    const T restSizeInv = (T)1.0 / restSize;
+    const T restSizeAdjust = (T)restSize / restSizeMinusOne;
 
     if(isTraining) {
         NDArray<T>* sum = xAffected.sum({0});
-        *mean = (*sum) * restSizeInv;            
+        mean->assign((*sum) * restSizeInv);
+        *batchMean = *mean;
         delete sum;
     }
-    *batchMean = *mean;
+    else 
+        *batchMean = 0.;
     
     xAffected -= *mean;
 
     if(isTraining) {        
         T power = 2.;
         NDArray<T>* sum = xAffected.template transform<simdOps::Pow<T>>(&power).sum({0});
-        *variance = (*sum) * restSizeInv;        
+        variance->assign((*sum) * restSizeInv);
         *batchVar  = (*variance) * restSizeAdjust;
         delete sum;
     }
     else 
-        *batchVar  = *variance;      
+        *batchVar  = 0.;      
 
     y->assign( xAffected * (*variance + epsilon).template transform<simdOps::RSqrt<T>>() * (*scale) + (*offset) );    
 
@@ -112,7 +108,7 @@ DECLARE_SHAPE_FN(fused_batch_norm) {
     
     COPY_SHAPE(xShapeInfo, outShapeInfo);
     COPY_SHAPE(scaleShapeInfo, batchMeanShapeInfo);
-    COPY_SHAPE(scaleShapeInfo, batchVarShapeInfo);
+    COPY_SHAPE(scaleShapeInfo, batchVarShapeInfo);    
     
     return new ShapeList({outShapeInfo, batchMeanShapeInfo, batchVarShapeInfo});                
 }
