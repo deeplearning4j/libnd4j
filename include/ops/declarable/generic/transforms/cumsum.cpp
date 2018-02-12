9332e71c7e119372a3c8127fcced297113a62e4d
raver119 <raver119@gmail.com>
2018-02-12 14:57:23 +0300

    additional prefix args

--- include/ops/declarable/generic/transforms/cumsum.cpp
+++ include/ops/declarable/generic/transforms/cumsum.cpp
@@ -7,17 +7,20 @@
 
 namespace nd4j {
     namespace ops {
-        CONFIGURABLE_OP_IMPL(cumsum, 1, 1, true, 0, -2) {
+        CONFIGURABLE_OP_IMPL(cumsum, 1, 1, true, 0, 2) {
             auto input = INPUT_VARIABLE(0);
             auto output = OUTPUT_VARIABLE(0);
 
+            const bool exclusive = INT_ARG(0) == 1;
+            const bool reverse = INT_ARG(1) == 1;
+
             if (block.getIArguments()->size() == 0 && block.width() == 1) {
                 // all at once case
-                nd4j::ops::helpers::_prefix<T, simdOps::Add<T>>(input->buffer(), input->shapeInfo(), output->buffer(), output->shapeInfo());
+                nd4j::ops::helpers::_prefix<T, simdOps::Add<T>>(input->buffer(), input->shapeInfo(), output->buffer(), output->shapeInfo(), exclusive, reverse);
             } else {
                 std::vector<int> dims = *(block.getIArguments());
 
-                nd4j::ops::helpers::_prefix<T, simdOps::Add<T>>(input, output, dims);
+                nd4j::ops::helpers::_prefix<T, simdOps::Add<T>>(input, output, dims, exclusive, reverse);
             }
 
             return ND4J_STATUS_OK;
--- include/ops/declarable/helpers/cpu/prefix.cpp
+++ include/ops/declarable/helpers/cpu/prefix.cpp
@@ -12,45 +12,82 @@ namespace nd4j {
     namespace ops {
         namespace helpers {
             template <typename T, typename OpName>
-            void _prefix(T* x, int* xShapeInfo, T* z, int* zShapeInfo) {
-                auto length = shape::length(xShapeInfo);
+            void _prefix(T* x, int* xShapeInfo, T* z, int* zShapeInfo, bool exclusive, bool reverse) {
+                auto length = (int) shape::length(xShapeInfo);
 
-                if (shape::elementWiseStride(xShapeInfo) == 1 && shape::elementWiseStride(zShapeInfo) == 1 && shape::order(xShapeInfo) == 'c' && shape::order(zShapeInfo) == 'c') {
-                    T sum = (T) 0;                    
-#pragma omp simd                    
-                    for (int e = 0; e < length; e++) {
-                        sum = OpName::op(sum, x[e]);
-                        z[e] = sum;
+                if (reverse) {
+                    if (shape::elementWiseStride(xShapeInfo) == 1 && shape::elementWiseStride(zShapeInfo) == 1 &&
+                        shape::order(xShapeInfo) == 'c' && shape::order(zShapeInfo) == 'c') {
+                        T sum = (T) 0;
+#pragma omp simd
+                        for (int e = length - 1; e >= 0; --e) {
+                            sum = OpName::op(sum, x[e]);
+                            z[e] = sum;
+                        }
+                    } else {
+                        int xCoord[MAX_RANK];
+                        int zCoord[MAX_RANK];
+                        T sum = (T) 0;
+
+                        int xRank = shape::rank(xShapeInfo);
+                        int zRank = shape::rank(zShapeInfo);
+
+                        int *xShape = shape::shapeOf(xShapeInfo);
+                        int *zShape = shape::shapeOf(zShapeInfo);
+
+                        int *xStride = shape::stride(xShapeInfo);
+                        int *zStride = shape::stride(zShapeInfo);
+
+                        for (int e = length - 1; e >= 0; --e) {
+                            shape::ind2subC(xRank, xShape, e, xCoord);
+                            shape::ind2subC(zRank, zShape, e, zCoord);
+
+                            Nd4jIndex xOffset = shape::getOffset(0, xShape, xStride, xCoord, xRank);
+                            Nd4jIndex zOffset = shape::getOffset(0, zShape, zStride, zCoord, zRank);
+
+                            sum = OpName::op(sum, x[xOffset]);
+                            z[zOffset] = sum;
+                        }
                     }
                 } else {
-                    int xCoord[MAX_RANK];
-                    int zCoord[MAX_RANK];
-                    T sum = (T) 0;  
+                    if (shape::elementWiseStride(xShapeInfo) == 1 && shape::elementWiseStride(zShapeInfo) == 1 &&
+                        shape::order(xShapeInfo) == 'c' && shape::order(zShapeInfo) == 'c') {
+                        T sum = (T) 0;
+#pragma omp simd
+                        for (int e = 0; e < length; e++) {
+                            sum = OpName::op(sum, x[e]);
+                            z[e] = sum;
+                        }
+                    } else {
+                        int xCoord[MAX_RANK];
+                        int zCoord[MAX_RANK];
+                        T sum = (T) 0;
 
-                    int xRank = shape::rank(xShapeInfo);
-                    int zRank = shape::rank(zShapeInfo);
+                        int xRank = shape::rank(xShapeInfo);
+                        int zRank = shape::rank(zShapeInfo);
 
-                    int *xShape = shape::shapeOf(xShapeInfo);
-                    int *zShape = shape::shapeOf(zShapeInfo);
+                        int *xShape = shape::shapeOf(xShapeInfo);
+                        int *zShape = shape::shapeOf(zShapeInfo);
 
-                    int *xStride = shape::stride(xShapeInfo);
-                    int *zStride = shape::stride(zShapeInfo);
+                        int *xStride = shape::stride(xShapeInfo);
+                        int *zStride = shape::stride(zShapeInfo);
 
-                    for (int e = 0; e < length; e++) {
-                        shape::ind2subC(xRank, xShape, e, xCoord);
-                        shape::ind2subC(zRank, zShape, e, zCoord);
+                        for (int e = 0; e < length; e++) {
+                            shape::ind2subC(xRank, xShape, e, xCoord);
+                            shape::ind2subC(zRank, zShape, e, zCoord);
 
-                        Nd4jIndex xOffset = shape::getOffset(0, xShape, xStride, xCoord, xRank);
-                        Nd4jIndex zOffset = shape::getOffset(0, zShape, zStride, zCoord, zRank);
+                            Nd4jIndex xOffset = shape::getOffset(0, xShape, xStride, xCoord, xRank);
+                            Nd4jIndex zOffset = shape::getOffset(0, zShape, zStride, zCoord, zRank);
 
-                        sum = OpName::op(sum, x[xOffset]);
-                        z[zOffset] = sum;
+                            sum = OpName::op(sum, x[xOffset]);
+                            z[zOffset] = sum;
+                        }
                     }
                 }
             };
 
             template <typename T, typename OpName>
-            void _prefix(NDArray<T>* x, NDArray<T>* z, std::vector<int>& dims) {
+            void _prefix(NDArray<T>* x, NDArray<T>* z, std::vector<int>& dims, bool exclusive, bool reverse) {
                 auto xTads = NDArrayFactory<T>::allTensorsAlongDimension(x, dims);
                 auto zTads = NDArrayFactory<T>::allTensorsAlongDimension(z, dims);
                 int t = xTads->size();
@@ -60,29 +97,29 @@ namespace nd4j {
                     auto tx = xTads->at(e);
                     auto tz = zTads->at(e);
 
-                    _prefix<T, OpName>(tx->buffer(), tx->shapeInfo(), tz->buffer(), tz->shapeInfo());
+                    _prefix<T, OpName>(tx->buffer(), tx->shapeInfo(), tz->buffer(), tz->shapeInfo(), exclusive, reverse);
                 }
 
                 delete xTads;
                 delete zTads;
             };
 
-            template void _prefix<float, simdOps::Add<float>>(float* x, int* xShapeInfo, float* z, int* zShapeInfo);
-            template void _prefix<float16, simdOps::Add<float16>>(float16* x, int* xShapeInfo, float16* z, int* zShapeInfo);
-            template void _prefix<double, simdOps::Add<double>>(double* x, int* xShapeInfo, double* z, int* zShapeInfo);
+            template void _prefix<float, simdOps::Add<float>>(float* x, int* xShapeInfo, float* z, int* zShapeInfo, bool exclusive, bool reverse);
+            template void _prefix<float16, simdOps::Add<float16>>(float16* x, int* xShapeInfo, float16* z, int* zShapeInfo, bool exclusive, bool reverse);
+            template void _prefix<double, simdOps::Add<double>>(double* x, int* xShapeInfo, double* z, int* zShapeInfo, bool exclusive, bool reverse);
 
-            template void _prefix<float, simdOps::Multiply<float>>(float* x, int* xShapeInfo, float* z, int* zShapeInfo);
-            template void _prefix<float16, simdOps::Multiply<float16>>(float16* x, int* xShapeInfo, float16* z, int* zShapeInfo);
-            template void _prefix<double, simdOps::Multiply<double>>(double* x, int* xShapeInfo, double* z, int* zShapeInfo);
+            template void _prefix<float, simdOps::Multiply<float>>(float* x, int* xShapeInfo, float* z, int* zShapeInfo, bool exclusive, bool reverse);
+            template void _prefix<float16, simdOps::Multiply<float16>>(float16* x, int* xShapeInfo, float16* z, int* zShapeInfo, bool exclusive, bool reverse);
+            template void _prefix<double, simdOps::Multiply<double>>(double* x, int* xShapeInfo, double* z, int* zShapeInfo, bool exclusive, bool reverse);
 
 
-            template void _prefix<float, simdOps::Add<float>>(NDArray<float>* x, NDArray<float>* z, std::vector<int>& dims);
-            template void _prefix<float16, simdOps::Add<float16>>(NDArray<float16>* x, NDArray<float16>* z, std::vector<int>& dims);
-            template void _prefix<double, simdOps::Add<double>>(NDArray<double>* x, NDArray<double>* z, std::vector<int>& dims);
+            template void _prefix<float, simdOps::Add<float>>(NDArray<float>* x, NDArray<float>* z, std::vector<int>& dims, bool exclusive, bool reverse);
+            template void _prefix<float16, simdOps::Add<float16>>(NDArray<float16>* x, NDArray<float16>* z, std::vector<int>& dims, bool exclusive, bool reverse);
+            template void _prefix<double, simdOps::Add<double>>(NDArray<double>* x, NDArray<double>* z, std::vector<int>& dims, bool exclusive, bool reverse);
 
-            template void _prefix<float, simdOps::Multiply<float>>(NDArray<float>* x, NDArray<float>* z, std::vector<int>& dims);
-            template void _prefix<float16, simdOps::Multiply<float16>>(NDArray<float16>* x, NDArray<float16>* z, std::vector<int>& dims);
-            template void _prefix<double, simdOps::Multiply<double>>(NDArray<double>* x, NDArray<double>* z, std::vector<int>& dims);
+            template void _prefix<float, simdOps::Multiply<float>>(NDArray<float>* x, NDArray<float>* z, std::vector<int>& dims, bool exclusive, bool reverse);
+            template void _prefix<float16, simdOps::Multiply<float16>>(NDArray<float16>* x, NDArray<float16>* z, std::vector<int>& dims, bool exclusive, bool reverse);
+            template void _prefix<double, simdOps::Multiply<double>>(NDArray<double>* x, NDArray<double>* z, std::vector<int>& dims, bool exclusive, bool reverse);
         }
     }
 }
