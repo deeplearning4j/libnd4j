fac0e16aa2f9254f0f4918783bac1fe7fef0c174
raver119@gmail.com <raver119@gmail.com>
2018-02-14 00:59:27 +0300

    random ops + some tests

---
+++ include/ops/declarable/generic/random/exponential.cpp
@@ -0,0 +1,38 @@
+//
+//
+//
+
+#include <ops/declarable/headers/random.h>
+
+namespace nd4j {
+    namespace ops {
+        CUSTOM_OP_IMPL(random_exponential, 1, 1, true, 1, 0) {
+            // uniform distribution
+            auto rng = block.getRNG();
+
+            if (rng == nullptr)
+                return Status::THROW("RNG is null, aborting...");
+
+            auto x = INPUT_VARIABLE(0);
+            auto z = OUTPUT_VARIABLE(0);
+
+            functions::random::RandomFunction<T>::template execTransform<randomOps::ExponentialDistribution<T>>(block.getRNG(), z->getBuffer(), z->getShapeInfo(), block.getTArguments()->data());
+
+            STORE_RESULT(*z);
+
+            return ND4J_STATUS_OK;
+        }
+
+
+        DECLARE_SHAPE_FN(random_exponential) {
+            auto in = INPUT_VARIABLE(0);
+            auto shape = in->template asVectorT<int>();
+
+            int *newShape;
+            ALLOCATE(newShape, block.getWorkspace(), shape::shapeInfoLength(shape.size()), int);
+            shape::shapeBuffer(shape.size(), shape.data(), newShape);
+
+            return new ShapeList(newShape);
+        }
+    }
+}
