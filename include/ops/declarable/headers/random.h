79887044c7c6d7df11702a699d8394b64d1efbac
raver119 <raver119@gmail.com>
2018-02-13 09:56:21 +0300

    uniform is custom now

--- include/ops/declarable/headers/random.h
+++ include/ops/declarable/headers/random.h
@@ -10,6 +10,6 @@ namespace nd4j {
         DECLARE_CUSTOM_OP(set_seed, -2, 1, false, 0, -2);
         DECLARE_CUSTOM_OP(get_seed, -2, 1, false, 0, 0);
        
-        DECLARE_CONFIGURABLE_OP(randomuniform, 1, 1, true, 2, 0);        
+        DECLARE_CUSTOM_OP(randomuniform, 1, 1, true, 2, 0);
     }
 }
