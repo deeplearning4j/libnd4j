f4032d37c8e5e38b9332d6935cc2885877161940
shugeo <sgazeos@gmail.com>
2018-02-22 16:07:11 +0200

    bincount op and tests. Working version.

--- include/ops/declarable/generic/parity_ops/bincount.cpp
+++ include/ops/declarable/generic/parity_ops/bincount.cpp
@@ -7,33 +7,37 @@
 
 namespace nd4j {
     namespace ops {
-        CUSTOM_OP_IMPL(bincount, 1, 1, false, 0, 0)
+        CUSTOM_OP_IMPL(bincount, 1, 1, false, 0, 0) {
+
             NDArray<T>* values = INPUT_VARIABLE(0);
             
-            NDArray<T>* weights = nullptr; //INPUT_VARIABLE(1);
+            NDArray<T>* weights;
             if (block.width() > 1) {
                 weights = INPUT_VARIABLE(1);
-                REQUIRE_TRUE(input->isSameShape(weights), 0, "bincount: the input and weights shapes should be equals");
+                REQUIRE_TRUE(values->isSameShape(weights), 0, "bincount: the input and weights shapes should be equals");
             }
             int maxLength = -1;
             int minLength = 0;
-            int maxIndex = input->argMax();
-            maxLength = int((*input)(maxIndex))  + 1;
+            int maxIndex = values->argMax();
+            maxLength = int((*values)(maxIndex))  + 1;
 
             if (block.numI() > 0) {
-                minLength = nd4j::math::max(INT_ARG(0), 0);
+                minLength = nd4j::math::nd4j_max(INT_ARG(0), 0);
                 if (block.numI() == 2) 
-                    maxLength = nd4j::math::max(maxLenght, INT_ARG(1));
+                    maxLength = nd4j::math::nd4j_min(maxLength, INT_ARG(1));
             }
 
             NDArray<T>* result = OUTPUT_VARIABLE(0);
-            
-            for (int e = 0; e < input->lengthOf(); e++) {
-                int val = (*input)(e);
-                if (val < maxLenght) {
-                    (*result)(val)++;
-                    if (weights != nullptr)
+            result->assign((T)0.0);
+
+            for (int e = 0; e < values->lengthOf(); e++) {
+                int val = (*values)(e);
+                if (val < maxLength) {
+                    if (block.width() > 1)
                         (*result)(val) += (*weights)(e);
+                    else
+                        (*result)(val)++;
+
                 }
             }
             return ND4J_STATUS_OK;
@@ -43,17 +47,17 @@ namespace nd4j {
             auto shapeList = new ShapeList(); 
             auto in = INPUT_VARIABLE(0);
 
-            int maxIndex = input->argMax();
-            int maxLength = int((*input)(maxIndex))  + 1;
+            int maxIndex = in->argMax();
+            int maxLength = int((*in)(maxIndex))  + 1;
 
             if (block.numI() > 1) 
-                maxLength = nd4j::math::max(maxLenght, INT_ARG(1));
+                maxLength = nd4j::math::nd4j_min(maxLength, INT_ARG(1));
 
             int* newshape;
             
-            ALLOCATE(newshape, block.getWorkspace(), shape::shapeInfoLength(shapeRank), int);
+            ALLOCATE(newshape, block.getWorkspace(), shape::shapeInfoLength(1), int);
 
-            shape::shapeVector(maxLenght,  newshape);
+            shape::shapeVector(maxLength,  newshape);
 
             shapeList->push_back(newshape); 
             return shapeList;
