634d3819c09b5446295256f05f3c3ebbf0ea4736
shugeo <sgazeos@gmail.com>
2018-03-01 03:32:17 +0200

    Fixed inverse to worked using LUP processing

--- include/ops/declarable/helpers/cpu/lup.cpp
+++ include/ops/declarable/helpers/cpu/lup.cpp
@@ -11,8 +11,9 @@ namespace helpers {
 
     template <typename T> 
     void swapRows(NDArray<T>* matrix, int theFirst, int theSecond) {
+
         if (theFirst != theSecond)
-        for (int i = 0; i = matrix->columns(); i++) {
+        for (int i = 0; i < matrix->columns(); i++) {
             std::swap((*matrix)(theFirst, i), (*matrix)(theSecond, i));
         }
     }
@@ -23,9 +24,12 @@ namespace helpers {
     template <typename T>
     void invertLowerMatrix(NDArray<T>* inputMatrix, NDArray<T>* invertedMatrix) {
         int n = inputMatrix->rows();
+        invertedMatrix->assign(T(0.0));
 
         for (int i = 0; i < n; i++)
             (*invertedMatrix)(i, i) = T(1.0);
+
+        if (inputMatrix->isIdentityMatrix()) return;
 
         for (int i = 1; i < n; i++)
             (*invertedMatrix)(i, i - 1) = -(*inputMatrix)(i, i - 1);
@@ -44,19 +48,21 @@ namespace helpers {
     template <typename T>
     void invertUpperMatrix(NDArray<T>* inputMatrix, NDArray<T>* invertedMatrix) {
         int n = inputMatrix->rows();
+        invertedMatrix->assign(T(0.0));
 
         for (int i = 0; i < n; i++)
             (*invertedMatrix)(i, i) =  T(1.0) / (*inputMatrix)(i, i);
 
+        if (inputMatrix->isIdentityMatrix()) return;
+
         for (int i = 0; i < n - 1; i++)
-            (*invertedMatrix)(i, i + 1) = -(*inputMatrix)(i, i - 1) / ((*inputMatrix)(i,i) * (*inputMatrix)(i+1, i + 1));
+            (*invertedMatrix)(i, i + 1) -= (*inputMatrix)(i, i + 1) / ((*inputMatrix)(i, i) * (*inputMatrix)(i + 1, i + 1));
 
         for (int i = n - 2; i > - 1; i--) {
             for (int j = i + 1; j < n; j++) 
-                for (int k = i; k < n; k++) 
+                for (int k = i + 1; k < n; k++) 
                     (*invertedMatrix)(i, j) -= (*invertedMatrix)(k, j) * (*inputMatrix)(i, k) / (*inputMatrix)(i, i);
         }
-
     }
 
     template void invertUpperMatrix(NDArray<float>* inputMatrix, NDArray<float>* invertedMatrix);
@@ -75,11 +81,10 @@ namespace helpers {
         permutationMatrix->setIdentity();
         
         for(int i = 0; i < rowNum; i++ ) {
-
-            T pivotValue = 0;
+            T pivotValue = T(0.0);
             int pivot = -1;
 
-            for( int rowCounter = i; rowCounter < rowNum; rowCounter++ ) {
+            for(int rowCounter = i; rowCounter < rowNum; rowCounter++ ) {
                 if(nd4j::math::nd4j_abs((*compoundMatrix)(rowCounter, i)) > pivotValue ) {
                     pivotValue = nd4j::math::nd4j_abs((*compoundMatrix)(rowCounter, i));
                     pivot = rowCounter;
@@ -89,16 +94,15 @@ namespace helpers {
             if( pivotValue != T(0.0) ) {
                 swapRows(compoundMatrix.get(), pivot, i);
                 swapRows(permutationMatrix.get(), pivot, i);
-
                 for( int j = i + 1; j < rowNum; j++ ) {
                     (*compoundMatrix)(j, i) /= (*compoundMatrix)(i, i);
                     for( int k = i + 1; k < rowNum; k++ ) 
-                        (*compoundMatrix)(j , k) -= (*compoundMatrix)(j, i) * (*compoundMatrix)(i, k);
+                        (*compoundMatrix)(j, k) -= (*compoundMatrix)(j, i) * (*compoundMatrix)(i, k);
                 }
 
             }
         }
-    
+
         for (int e = 0; e < rowNum; e++)
             determinant *= (*compoundMatrix)(e, e);
 
@@ -144,7 +148,7 @@ namespace helpers {
 
         int n = input->sizeAt(-1);
         int n2 = n * n;
-        int totalCount = output->lengthOf() / n;
+        int totalCount = output->lengthOf() / n2;
 
         std::unique_ptr<NDArray<T>> matrix(new NDArray<T>({n, n})); //, block.getWorkspace());
         std::unique_ptr<NDArray<T>> compound(new NDArray<T>({n, n})); //, block.getWorkspace());
@@ -154,30 +158,37 @@ namespace helpers {
 
         for (int e = 0; e < totalCount; e++) {
 
-
+            for (int k = e * n2, row = 0; k < (e + 1) * n2; k++) {
+                (*matrix)(row++) = (*input)(k);
+            }
             T det = lup(matrix.get(), compound.get(), permutation.get());
 
             if (nd4j::math::nd4j_abs(det) < T(0.0000001)) {
-                nd4j_printf("matrix_inverse: The matrix %i has no inverse. Quiting...\n", e);
+                nd4j_printf("matrix_inverse: The matrix %i has no inverse due determinant is %lf. Quiting...\n", e, det);
+                matrix->printIndexedBuffer("Wrong matrix");
                 ND4J_STATUS_VALIDATION;
             }
-
             lowerMatrix->setIdentity(); // set up U to identity matrix
             for (int k = 1; k < n; k++) {  // and then put all values under main diagonal on to it
                 for (int j = 0; j < k; j++)
                     (*lowerMatrix)(k, j) = (*compound)(k, j);
             }
+            upperMatrix->setIdentity(); // set up U to identity matrix
+            for (int k = 0; k < n; k++) {  // and then put all values under main diagonal on to it
+                for (int j = k; j < n; j++)
+                    (*upperMatrix)(k, j) = (*compound)(k, j);
+            }
 
             invertUpperMatrix(upperMatrix.get(), matrix.get());
             invertLowerMatrix(lowerMatrix.get(), upperMatrix.get());
-            
+
             nd4j::NDArrayFactory<T>::mmulHelper(matrix.get(), upperMatrix.get(), compound.get(), T(1.0f), T(0.0f));
             nd4j::NDArrayFactory<T>::mmulHelper(compound.get(), permutation.get(), matrix.get(), T(1.0f), T(0.0f));
 
             for (int k = e * n2, row = 0; k < (e + 1) * n2; k++) {
                 (*output)(k) = (*matrix)(row++);
             }
-
+            matrix->assign((T)0.0);
         }
 
         return ND4J_STATUS_OK;
