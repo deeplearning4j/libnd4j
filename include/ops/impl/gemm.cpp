a5a8905a29ee50bbbf766fb175265180a48183e1
shugeo <sgazeos@gmail.com>
2018-03-12 12:20:27 +0200

    Changed implementation for gemm. Restored athor mark for matmul.

--- include/ops/impl/gemm.cpp
+++ include/ops/impl/gemm.cpp
@@ -45,19 +45,16 @@ namespace nd4j {
                        T beta,
                        T *C, int ldc) {
 
-            // M - row count for A
-            // N - column count for B
-            // K - column count for A
-            // C matrix has MxN dim
             // optionally handle transpose
             // we want C always here
-            bool transposeAFlag = TransA != 'N' && TransA != 'n';
-            bool transposeBFlag = TransB != 'N' && TransB != 'n';
-            //T *aT = transposeAFlag ? transpose(CblasColMajor, CblasRowMajor, M, K, A) : A;
+            bool transAFlag = TransA != CblasTrans;
+            bool transBFlag = TransB == CblasTrans;
+//            T *aT = transAFlag ? transpose(CblasColMajor, CblasRowMajor, M, K, A) : A;
 
             // we want F always here
-            //T *bT = transposeBFlag ? transpose(CblasRowMajor, CblasColMajor, K, N, B) : B;
+//            T *bT = transBFlag ? transpose(CblasRowMajor, CblasColMajor, K, N, B) : B;
 
+            printf("Matrix multiply with: Order %c, TransA %c, TransB %c, Row count %i, Output columns %i, A/B column/rows %i (%s,%s)\n", Order, TransA, TransB, M, N, K, (transAFlag?"TRUE":"FALSE"), (transBFlag?"TRUE":"FALSE"));
 
             if (beta == (T) 0.0f) {
                 int length = M*N;
@@ -72,71 +69,127 @@ namespace nd4j {
                 }
             }
 
-            int rowCount = (transposeAFlag?K:M);
-            int rowCountB = (transposeBFlag?N:K);
-            int columnCountA = (transposeAFlag?M:K);
-            int columnCount = (transposeBFlag?K:N);
-            int strideA = (transposeAFlag?columnCountA:1);
-            int strideB = (transposeBFlag?1:columnCount);
-
-            printf("Row count %i, Column count %i, B rows %i, A columns %i, lda %i, ldb %i\n", rowCount, columnCount, rowCountB, columnCountA, lda, ldb);
-            printf("strideA %i, strideB %i\n", strideA, strideB);
-            for (int i = 0; i < rowCount; i++) {
-                for (int j = 0; j < columnCountA; j++)
-                    printf("%010.2lf  ", (double)A[i * K + j]);
-                printf("\n");
-            }
-            if (rowCountB != columnCountA) {
-                printf("Something wrong with input matricies: %i != %i\n", columnCountA, rowCountB);
-            }
-            printf("\n==========================================\n");
-            for (int i = 0; i < rowCountB; i++) {
-                for (int j = 0; j < columnCount; j++)
-                    printf("%010.2lf  ", (double)B[i * N + j]);
-                printf("\n");
-            }
 
 #pragma omp parallel for proc_bind(spread)
-            for (int r = 0; r < rowCount; r++) {
-
-                int aIdx = 0; //linearIndexC(rowCount, columnCount, r, 0);
-                //T *aX = aT + aIdx;
-                 
-                printf("r = %i, aIdx = %i from %i\n", r, aIdx, rowCount * columnCountA);
-                
-                for (int c = 0; c < columnCount; c++) {
-                    int zIdx = linearIndexF(rowCount, columnCount, r, c);
-
-                    T dot = (T) 0.0f;
-
-                    if (alpha != (T) 0.0f) {
-                        int bIdx = 0; //-strideB; //linearIndexF(columnCount, columnCount, 0, c);
-                        printf("c = %i, bIdx = %i from %i\n", c, bIdx, rowCountB * columnCount);
-
-                        //T *bX = bT + bIdx;
-                        for (int e = 0; e < rowCountB; e++) {
-                            aIdx = e * strideA + r;
-                            bIdx = e * strideB + c;// * e;
-                            printf("%i: aIdx = %i, bIdx = %i\n", e, aIdx, bIdx);
-
-                            dot += A[aIdx] * B[bIdx];
-//                            dot = nd4j::math::nd4j_dot<T>(aX, bX, K) * alpha;
+            for (int r = 0; r < M; r++) {
+                if (!transAFlag && !transBFlag) { // matricies already in proper
+                    int aIdx = linearIndexC(M, K, r, 0);
+                    T *aX = A + aIdx;
+    
+                    for (int c = 0; c < N; c++) {
+                        int zIdx = linearIndexF(M, N, r, c);
+    
+                        T dot = (T) 0.0f;
+    
+                        if (alpha != (T) 0.0f) {
+                            int bIdx = linearIndexF(K, N, 0, c);
+    
+                            T *bX = B + bIdx;
+    
+                            dot = nd4j::math::nd4j_dot<T>(aX, bX, K) * alpha;
+                        }
+    
+                        if (beta != (T) 0.0f) {
+                            C[zIdx] = dot + beta * C[zIdx];
+                        } else {
+                            C[zIdx] = dot;
                         }
                     }
-                    if (beta != (T) 0.0f) {
-                        C[zIdx] = dot + beta * C[zIdx];
-                    } else {
-                        C[zIdx] = dot;
-                    }
                 }
+                else if (transAFlag && !transBFlag) { // matrix A need to be viewed as transposed              else {
+                    int aIdx;// = linearIndexC(K, M, r, 0);
+                    for (int c = 0; c < N; c++) {
+                        int zIdx = linearIndexF(M, N, r, c);
+    
+                        T dot = (T) 0.0f;
+    
+                        if (alpha != (T) 0.0f) {
+                            int bIdx = linearIndexF(K, N, 0, c);
+    
+                            T *bX = B + bIdx;
+                            //                    int aIdx = linearIndexC(K, M, r, 0);
+                            for (int k = 0; k < K; k++) {
+                                aIdx = linearIndexF(M, K, r, k);
+
+                                dot += alpha * A[aIdx] * (bX[k]);//A[aIdx]nd4j::math::nd4j_dot<T>(aX, bX, K) * alpha;
+                            }
+                        }
+    
+                        if (beta != (T) 0.0f) {
+                            C[zIdx] = dot + beta * C[zIdx];
+                        } else {
+                            C[zIdx] = dot;
+                        }
+                    }
+
+                }
+                else if (!transAFlag && transBFlag) { // matrix B need to be viewed as transposed
+                    int aIdx = linearIndexC(M, K, r, 0);
+                    T *aX = A + aIdx;
+
+                    for (int c = 0; c < N; c++) {
+                        int zIdx = linearIndexF(M, N, r, c);
+    
+                        T dot = (T) 0.0f;
+    
+                        if (alpha != (T) 0.0f) {
+                            int bIdx;// = linearIndexF(K, N, 0, c);
+    
+                            T *bX = B + bIdx;
+                            //                    int aIdx = linearIndexC(K, M, r, 0);
+                            for (int k = 0; k < K; k++) {
+                                bIdx = linearIndexC(K, M, r, k);
+
+                                dot += alpha * aX[k] * B[bIdx];//A[aIdx]nd4j::math::nd4j_dot<T>(aX, bX, K) * alpha;
+                            }
+                        }
+    
+                        if (beta != (T) 0.0f) {
+                            C[zIdx] = dot + beta * C[zIdx];
+                        } else {
+                            C[zIdx] = dot;
+                        }
+                    }
+
+
+                }
+                else { // both matricies need to be viewed as transposed
+                    int aIdx;
+                    for (int c = 0; c < N; c++) {
+                        int zIdx = linearIndexF(M, N, r, c);
+    
+                        T dot = (T) 0.0f;
+    
+                        if (alpha != (T) 0.0f) {
+                            int bIdx; // = linearIndexF(K, N, 0, c);
+    
+                            //T *bX = bT + bIdx;
+                            //                    int aIdx = linearIndexC(K, M, r, 0);
+                            for (int k = 0; k < K; k++) {
+                                aIdx = linearIndexF(K, M, r, k);
+                                bIdx = linearIndexC(K, N, k, c);
+                                dot += alpha * A[aIdx] * B[k];//A[aIdx]nd4j::math::nd4j_dot<T>(aX, bX, K) * alpha;
+                            }
+                        }
+    
+                        if (beta != (T) 0.0f) {
+                            C[zIdx] = dot + beta * C[zIdx];
+                        } else {
+                            C[zIdx] = dot;
+                        }
+                    }
+
+
+                }
+
             }
 
 
             // if transpose was applied - dismiss transposed arrays
-//            if (transposeAFlag)
+//            if (transAFlag)
 //                delete[] aT;
 
-//            if (transposeBFlag == CblasTrans)
+//            if (transBFlag)
 //                delete[] bT;
         }
 
