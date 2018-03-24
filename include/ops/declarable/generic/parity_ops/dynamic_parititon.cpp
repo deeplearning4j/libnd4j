a1943dfac05c7fccd7fad249993164efa5e3a39b
raver119@gmail.com <raver119@gmail.com>
2018-03-24 16:58:02 -0700

    - transpose TF-mode - Node now tracks references - inPlace tagging fixed

--- include/ops/declarable/generic/parity_ops/dynamic_parititon.cpp
+++ include/ops/declarable/generic/parity_ops/dynamic_parititon.cpp
@@ -10,6 +10,7 @@ namespace ops {
     CUSTOM_OP_IMPL(dynamic_partition, 2, 1, false, 0, 1) {
         NDArray<T>* input = INPUT_VARIABLE(0);
         NDArray<T>* indices = INPUT_VARIABLE(1);
+
         //NDArray<T>* indexVector = ; // linearize to actualize
         REQUIRE_TRUE(input->rankOf() >= indices->rankOf(), 0, "dynamic_partition: data tensor rank should be non-lesser than indices\' tensor, but %i < %i given,",
             input->rankOf(), indices->rankOf());
