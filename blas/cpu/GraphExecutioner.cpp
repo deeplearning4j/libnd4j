3108460323dfb4e3483e55fa7b55bf447f9910e0
raver119@gmail.com <raver119@gmail.com>
2018-03-25 13:18:31 -0700

    special commit for Yurii

--- blas/cpu/GraphExecutioner.cpp
+++ blas/cpu/GraphExecutioner.cpp
@@ -444,6 +444,7 @@ Nd4jStatus GraphExecutioner<T>::execute(Graph<T> *graph, VariableSpace<T>* varia
                     auto array = __variableSpace->getVariable(node->id())->getNDArray();
                     auto list = __variableSpace->getVariable(node->id())->hasNDArrayList() ? __variableSpace->getVariable(node->id())->getNDArrayList() : nullptr;
                     auto shape = ShapeUtils<T>::shapeAsString(*array);
+
                     if (array != nullptr) {
                         auto values = array->asIndexedString(16);
                         nd4j_debug("node_%i finished. result shape: %s; first values: %s\n", node->id(), shape.c_str(), values.c_str());
