7302ced924866290062c6e0fc291ce8b6e9dc92c
Yurii <yurii@skymind.io>
2018-04-01 19:56:54 +0300

    implementation and testing triu_bp

--- include/ops/declarable/generic/transforms/triu.cpp
+++ include/ops/declarable/generic/transforms/triu.cpp
@@ -48,26 +48,67 @@ CUSTOM_OP_IMPL(triu, 1, 1, false, 0, 0) {
 
 DECLARE_SHAPE_FN(triu) {
 
-	int* inShapeInfo = inputShape->at(0);
+	int* gradOShapeInfo = inputShape->at(0);
 
-    int rank = (inShapeInfo[0] == 1) ? 2 : inShapeInfo[0];
+    int rank = (gradOShapeInfo[0] == 1) ? 2 : gradOShapeInfo[0];
     
     int* outShapeInfo = nullptr;
 	ALLOCATE(outShapeInfo, block.getWorkspace(), shape::shapeInfoLength(rank), int);    
-    memcpy(outShapeInfo, inShapeInfo, (1 + rank) * sizeof(int));                     // copy rank and dimensions values only
+    memcpy(outShapeInfo, gradOShapeInfo, (1 + rank) * sizeof(int));                     // copy rank and dimensions values only
 
-    if(inShapeInfo[0] == 1) {
+    if(gradOShapeInfo[0] == 1) {
         outShapeInfo[0] = rank; 
-        outShapeInfo[1] = inShapeInfo[1];
-        outShapeInfo[2] = inShapeInfo[1];
+        outShapeInfo[1] = gradOShapeInfo[1];
+        outShapeInfo[2] = gradOShapeInfo[1];
     }
 
-	shape::updateStrides(outShapeInfo, shape::order(inShapeInfo));
+	shape::updateStrides(outShapeInfo, shape::order(gradOShapeInfo));
 
     return SHAPELIST(outShapeInfo);    
 }
 
 
+
+//////////////////////////////////////////////////////////////////////////
+CUSTOM_OP_IMPL(triu_bp, 2, 1, false, 0, 0) {
+    
+    NDArray<T>* input = INPUT_VARIABLE(0);
+    NDArray<T>* dLdO = INPUT_VARIABLE(1);
+
+    NDArray<T>* dLdI = OUTPUT_VARIABLE(0);              // dLoss/dI
+
+    const int diag = block.getIArguments()->size() > 0 ? INT_ARG(0) : 0;
+
+    nd4j::ops::triu<T> op;
+    ResultSet<T>* results = op.execute({input}, {}, {diag});
+    NDArray<T>* dOdI = results->at(0);                          // dO/dI
+    
+    for(int i = 0; i < dOdI->lengthOf(); ++i) {
+        T* currElement = &(*dOdI)(i);
+        if(*currElement != (T)0.)
+            *currElement = 1.;
+    }
+
+    dLdI->assign((*dOdI) * (*dLdO));                          // chain rule: dLoss/dI = dO/dI * dLoss/dO 
+
+    delete results;
+    return Status::OK();
+}
+
+
+DECLARE_SHAPE_FN(triu_bp) {
+
+    int* gradOShapeInfo = inputShape->at(1);
+    int rank = gradOShapeInfo[0];
+
+    int* outShapeInfo = nullptr;
+    ALLOCATE(outShapeInfo, block.getWorkspace(), shape::shapeInfoLength(rank), int);    
+    memcpy(outShapeInfo, gradOShapeInfo, (1 + rank) * sizeof(int));                     // copy rank and dimensions values only    
+
+    shape::updateStrides(outShapeInfo, shape::order(inputShape->at(0)));
+
+    return SHAPELIST(outShapeInfo);    
+}
 
 
 }
