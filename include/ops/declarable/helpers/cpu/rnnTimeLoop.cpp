c992f3257663838533ac4a7393675c3f5c63aca2
Yurii <yurii@skymind.io>
2018-04-06 13:00:43 +0300

    minor correction in rnnTimeLoop

--- include/ops/declarable/helpers/cpu/rnnTimeLoop.cpp
+++ include/ops/declarable/helpers/cpu/rnnTimeLoop.cpp
@@ -35,8 +35,7 @@ void rnnTimeLoop(const std::vector<NDArray<T>*>& inArrs, NDArray<T>* h, NDArray<
         *hFinal = 0.;   
 
     BlasHelper::getInstance();          // to avoid memory leak in pragma parallel loops
-// #pragma omp parallel for if(bS > Environment::getInstance()->elementwiseThreshold()) schedule(guided) 
-#pragma omp parallel for schedule(guided)     
+#pragma omp parallel for if(bS > Environment::getInstance()->elementwiseThreshold()) schedule(guided) 
     // loop through batch of inputs           
     for (int e = 0; e < bS; ++e) {              
         
