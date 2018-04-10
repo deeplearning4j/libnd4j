a8622a6a54f3998398962b210b6bb2609f2178e1
raver119@gmail.com <raver119@gmail.com>
2018-04-09 22:05:21 -0700

    transforms cpu

--- include/loops/transform.h
+++ include/loops/transform.h
@@ -208,9 +208,7 @@ template<typename OpType>
 #endif
 
 
-			static void exec(int opNum, T *dx, int xStride, T *result, int resultStride, T *extraParams, const int n) {
-                                DISPATCH_BY_OPNUM(exec, PARAMS(dx, xStride, result, resultStride, extraParams, n), TRANSFORM_OPS);
-			}
+			static void exec(int opNum, T *dx, int xStride, T *result, int resultStride, T *extraParams, const int n);
 
 			static void exec(
 				int opNum,
@@ -220,9 +218,7 @@ template<typename OpType>
 				int *resultShapeInfo,
 				T *extraParams,
 				int *indexes,
-				int *resultIndexes, int *tadShapeInfo, Nd4jIndex *tadOffsets) {
-                            DISPATCH_BY_OPNUM(exec, PARAMS(dx, xShapeInfo, result, resultShapeInfo, extraParams, indexes, resultIndexes, tadShapeInfo, tadOffsets), TRANSFORM_OPS);
-			}
+				int *resultIndexes, int *tadShapeInfo, Nd4jIndex *tadOffsets);
 
 
 			static void exec(
@@ -231,9 +227,7 @@ template<typename OpType>
 				int *xShapeInfo,
 				T *result,
 				int *resultShapeInfo,
-				T *extraParams, int *tadShapeInfo, Nd4jIndex *tadOffsets) {
-                                DISPATCH_BY_OPNUM(exec, PARAMS(dx, xShapeInfo, result, resultShapeInfo, extraParams, tadShapeInfo, tadOffsets), TRANSFORM_OPS);
-			}
+				T *extraParams, int *tadShapeInfo, Nd4jIndex *tadOffsets);
 
 
 			template<typename OpType>
@@ -242,62 +236,7 @@ template<typename OpType>
                     int *xShapeInfo,
                     T *result,
                     int *resultShapeInfo,
-                    T *extraParams, int *tadShapeInfo, Nd4jIndex *tadOffsets) {
-
-                if(OpType::requiresSpecial) {
-                    OpType::execSpecial(dx,xShapeInfo,result,resultShapeInfo,extraParams, tadShapeInfo, tadOffsets);
-                    return;
-                }
-
-                int n = shape::length(xShapeInfo);
-                int xElementWiseStride = shape::elementWiseStride(xShapeInfo);
-                int resultElementWiseStride = shape::elementWiseStride(resultShapeInfo);
-
-                if(xElementWiseStride >= 1 && resultElementWiseStride >= 1 && shape::order(xShapeInfo) == shape::order(resultShapeInfo)) {
-                    exec<OpType>(dx,xElementWiseStride,result,resultElementWiseStride,extraParams,n);
-                }
-                else {
-                    int shapeIter[MAX_RANK];
-                    int coord[MAX_RANK];
-                    int dim;
-                    int xStridesIter[MAX_RANK];
-                    int resultStridesIter[MAX_RANK];
-                    int *xShape = shape::shapeOf(xShapeInfo);
-                    int *xStride = shape::stride(xShapeInfo);
-                    int *resultStride = shape::stride(resultShapeInfo);
-                    int rank = shape::rank(xShapeInfo);
-                    if(PrepareTwoRawArrayIter<T>(rank,
-                                                 xShape,
-                                                 dx,
-                                                 xStride,
-                                                 result,
-                                                 resultStride,
-                                                 &rank,
-                                                 shapeIter,
-                                                 &dx,
-                                                 xStridesIter,
-                                                 &result,
-                                                 resultStridesIter) >= 0) {
-                        ND4J_RAW_ITER_START(dim, rank, coord, shapeIter);
-                        {
-                            // Process the innermost dimension
-                            T *xIter = dx;
-                            T *resultIter = result;
-                            resultIter[0] = OpType::op(xIter[0], extraParams);
-                        }
-                        ND4J_RAW_ITER_TWO_NEXT(dim,
-                                               rank,
-                                               coord,
-                                               shapeIter,
-                                               dx,
-                                               xStridesIter,
-                                               result,
-                                               resultStridesIter);
-
-                    }
-
-                }
-            }
+                    T *extraParams, int *tadShapeInfo, Nd4jIndex *tadOffsets);
 
 
 			template<typename OpType>
@@ -308,59 +247,7 @@ template<typename OpType>
 				int *resultShapeInfo,
 				T *extraParams,
 				int *indexes,
-				int *resultIndexes, int *tadShapeInfo, Nd4jIndex *tadOffsets) {
-
-				int n = shape::length(xShapeInfo);
-#pragma omp parallel for simd schedule(guided) proc_bind(AFFINITY) default(shared)
-				for (Nd4jIndex i = 0; i < n; i++) {
-					result[resultIndexes[i]] = OpType::op(dx[indexes[i]], extraParams);
-				}
-			}
-
-            template<typename OpType>
-            static void exec(T *dx,
-                             int xStride,
-                             T *result,
-                             int resultStride,
-                             T *extraParams,
-                             const int n) {
-
-                int elementsPerThread = n / ELEMENT_THRESHOLD;
-                int num_threads = nd4j::math::nd4j_max<int>(1, elementsPerThread);
-                num_threads = nd4j::math::nd4j_min<int>(num_threads, omp_get_max_threads());
-
-                int span = (n / num_threads) + 8;
-
-                if (xStride == 1 && resultStride == 1) {
-
-#pragma omp parallel num_threads(num_threads) if (num_threads>1) proc_bind(AFFINITY) default(shared)
-                    {
-                        int tid = omp_get_thread_num();
-                        int start = span * tid;
-                        int end = span * (tid + 1);
-                        if (end > n) end = n;
-
-#pragma omp simd
-                        for (Nd4jIndex i = start; i < end; i++) {
-                            result[i] = OpType::op(dx[i], extraParams);
-                        }
-                    }
-                } else {
-
-#pragma omp parallel num_threads(num_threads) if (num_threads>1) proc_bind(AFFINITY) default(shared)
-                    {
-                        int tid = omp_get_thread_num();
-                        int start = span * tid;
-                        int end = span * (tid + 1);
-                        if (end > n) end = n;
-
-#pragma omp simd
-                        for (Nd4jIndex i = start; i < end; i++) {
-                            result[i*resultStride] = OpType::op(dx[i * xStride], extraParams);
-                        }
-                    }
-                }
-            }
+				int *resultIndexes, int *tadShapeInfo, Nd4jIndex *tadOffsets);
         };
     }
 }
