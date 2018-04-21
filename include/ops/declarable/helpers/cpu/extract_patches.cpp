0a281b4d987595ebf498eb713ecd2d24f558760f
shugeo <sgazeos@gmail.com>
2018-04-21 04:46:44 +0300

    Modified patching.

--- include/ops/declarable/helpers/cpu/extract_patches.cpp
+++ include/ops/declarable/helpers/cpu/extract_patches.cpp
@@ -12,10 +12,21 @@ namespace helpers {
     template <typename T>
     void extractPatches(NDArray<T>* images, NDArray<T>* output, 
         int sizeRow, int sizeCol, int stradeRow, int stradeCol, int rateRow, int rateCol, bool theSame){
-        std::vector<int> restDims({3});
-        std::unique_ptr<ResultSet<T>> listOfTensors(NDArrayFactory<T>::allTensorsAlongDimension(images, restDims));
-        for (int i = 0; i < listOfTensors->size(); i++) {
-            listOfTensors->at(i)->printIndexedBuffer();
+//        std::vector<int> restDims({3});
+//        std::unique_ptr<ResultSet<T>> listOfTensors(NDArrayFactory<T>::allTensorsAlongDimension(images, restDims));
+        int row = 0;
+        int e = 0;
+        int ksizeRowsEffective = sizeRow + (sizeRow - 1) * (rateRow - 1);
+        int ksizeColsEffective = sizeCol + (sizeCol - 1) * (rateRow - 1);
+        int ksize = ksizeRowsEffective * ksizeColsEffective;
+        int count = images->lengthOf() / ksize;
+        int outputCount = output->lengthOf() / ksize;
+        nd4j_printf("Input count %i, output count %i.\n", count, outputCount);
+        for (int i = 0; i < images->lengthOf(); ) {
+                for (int j = 0; j < output->sizeAt(3); ++j) 
+                    (*output)(e++) = (*images)(i + j);
+            if (e >= output->lengthOf()) break;
+            i += ksize * images->sizeAt(3);
         }
 /*
         int e = 0;
