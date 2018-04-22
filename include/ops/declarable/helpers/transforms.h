0158e736758681dcbf2c7fc0a177a2ef47584c82
Yurii <yurii@skymind.io>
2018-04-22 21:06:46 +0300

    refactoring/optimization of pad, gatherND and gather ops

--- include/ops/declarable/helpers/transforms.h
+++ include/ops/declarable/helpers/transforms.h
@@ -24,6 +24,18 @@ namespace helpers {
 	template <typename T>
 	void randomShuffle(NDArray<T>& input, NDArray<T>& output, nd4j::random::RandomBuffer& rng, const bool isInplace);
     
+    // auxiliary function which serves for recursion purpose and is used in pad operation
+	template<typename T>
+	void recursiveLoopForPad(const int mode, NDArray<T>& input, const NDArray<T>& paddings, NDArray<T>& output, std::vector<int> dimensions, int dim, int inIdx, int outIdx);
+
+	template<typename T>
+	void invertPermutation(const NDArray<T>& input, NDArray<T>& output);
+
+	template<typename T>
+	void gatherND(NDArray<T>& input, NDArray<T>& indices, NDArray<T>& output);
+
+	template<typename T>
+	void gather(NDArray<T>* input, const NDArray<T>* indices, NDArray<T>* output, const std::vector<int>& intArgs);
 
 }
 }
