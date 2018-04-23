0b556d1ca39e22be3a2c3a202401c1c62ae2847f
raver119@gmail.com <raver119@gmail.com>
2018-04-23 09:19:25 -0700

    meh

--- tests_cpu/layers_tests/GraphTests.cpp
+++ tests_cpu/layers_tests/GraphTests.cpp
@@ -1544,7 +1544,7 @@ TEST_F(GraphTests, Test_Inplace_Outputs_1) {
     ASSERT_TRUE(exp.equalsTo(z));
 }
 
-TEST_F(GraphTests, Test_Inplace_Outputs_1) {
+TEST_F(GraphTests, Test_Inplace_Outputs_2) {
     NDArray<float> x('c', {2, 3}, {1.f, 2.f, 3.f, 4.f, 5.f, 6.f});
     NDArray<float> exp('c', {6}, {1.f, 2.f, 3.f, 4.f, 5.f, 6.f});
     NDArray<float> z('c', {3, 3});
