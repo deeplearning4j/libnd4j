7885a17fbca94a944ee6d02929c061f561e8bf1d
raver119 <raver119@gmail.com>
2018-05-13 19:02:32 +0000

    initial commit

---
+++ tests_cpu/layers_tests/MmapTests.cpp
@@ -0,0 +1,21 @@
+//
+// Created by raver on 5/13/2018.
+//
+
+#include "testlayers.h"
+#include <ops/declarable/CustomOperations.h>
+#include <NDArray.h>
+#include <NDArrayFactory.h>
+#include <NativeOps.h>
+
+using namespace nd4j;
+using namespace nd4j::graph;
+
+class MmapTests : public testing::Test {
+public:
+
+};
+
+TEST_F(MmapTests, Test_Basic_Mmap_1) {
+
+}
