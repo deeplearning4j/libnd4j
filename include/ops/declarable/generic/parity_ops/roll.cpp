8945edc4806145510491698aa7189320ec0e3cb7
shugeo <sgazeos@gmail.com>
2018-05-15 15:44:48 +0300

    roll op initial version and tests.

---
+++ include/ops/declarable/generic/parity_ops/roll.cpp
@@ -0,0 +1,65 @@
+//
+//  @author raver119@gmail.com
+//
+
+#include <op_boilerplate.h>
+#if NOT_EXCLUDED(OP_roll)
+
+#include <ops/declarable/headers/parity_ops.h>
+//#include <ops/declarable/helpers/cross.h>
+
+namespace nd4j {
+namespace ops {
+
+    CONFIGURABLE_OP_IMPL(roll, 1, 1, true, 0, 1) {
+
+        NDArray<T>* output = OUTPUT_VARIABLE(0);
+        NDArray<T>* input = INPUT_VARIABLE(0);
+        bool shiftIsLinear = true;
+        //std::vector<int> axes(input->rankOf());
+        int shift = INT_ARG(0);
+        if (block.numI() > 1)
+            shiftIsLinear = false;
+        if (shiftIsLinear) {
+            int fullLen = output->lengthOf();
+            if (shift > 0 && shift < fullLen) {
+                for (int e = 0; e < shift; ++e) {
+                    int sourceIndex = fullLen - shift + e;
+                    (*output)(e) = (*input)(sourceIndex);
+                }
+
+                for (int e = shift; e < fullLen; ++e) {
+                    (*output)(e) = (*input)(e - shift);
+                    nd4j_printf("%i(%i): %f\n", e, e - shift, (*output)(e));
+                }
+             }
+             else if (shift < 0 && shift > -fullLen) {
+                for (int e = 0; e < fullLen + shift; ++e) {
+                    (*output)(e) = (*input)(e - shift);
+                }
+                for (int e = fullLen + shift; e < fullLen; ++e) {
+                    (*output)(e) = (*input)(fullLen - e - shift);
+                }
+             }
+             else if (shift < -fullLen) {
+             }
+             else if (shift > fullLen) {
+             }
+             else
+                output->assign(input);
+        }
+        else {
+            std::vector<int> axes(block.numI() - 1);
+            for (unsigned e = 0; e < axes.size(); ++e) {
+                int axe = INT_ARG(e + 1);
+                REQUIRE_TRUE(axe < input->rankOf() && axe >= -input->rankOf(), 0, "roll: axe value should be between -%i and %i, but %i was given.",
+                    input->rankOf(), input->rankOf() - 1, axe);
+            }
+        }
+
+        return ND4J_STATUS_OK;
+    }
+}
+}
+
+#endif
--- tests_cpu/layers_tests/DeclarableOpsTests7.cpp
+++ tests_cpu/layers_tests/DeclarableOpsTests7.cpp
@@ -456,7 +456,6 @@ TEST_F(DeclarableOpsTests7, Test_Dynamic_Stitch_Prof_1) {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-
 TEST_F(DeclarableOpsTests7, Test_Dynamic_Stitch_119_1) {
     NDArray<float> indices0('c', {2}, {1.0f, 10.f});
     NDArray<float> indices1('c', {2, 3}, {0,7,9, 5,8,3});
@@ -1253,6 +1252,67 @@ NDArray<double> exp('c', {2, 1, 4, 4}, {
 
     delete result;
 }
+
+////////////////////////////////////////////////////////////////////////////////
+TEST_F(DeclarableOpsTests7, TestRoll_1) {
+    NDArray<double> x('c', {2, 2, 4, 2}, {
+    11.11, 11.12, 11.21, 11.22, 11.31, 11.32, 11.41, 11.42,     12.11, 12.12, 12.21, 12.22, 12.31, 12.32, 12.41, 12.42,
+    21.11, 21.12, 21.21, 21.22, 21.31, 21.32, 21.41, 21.42,     22.11, 22.12, 22.21, 22.22, 22.31, 22.32, 22.41, 22.42
+});
+
+NDArray<double> exp('c', {2, 2, 4, 2}, {
+    22.21, 22.22, 22.31, 22.32, 22.41, 22.42, 11.11, 11.12, 11.21, 11.22, 11.31, 11.32, 11.41, 11.42,     
+    12.11, 12.12, 12.21, 12.22, 12.31, 12.32, 12.41, 12.42, 21.11, 21.12, 21.21, 21.22, 21.31, 21.32, 
+    21.41, 21.42, 22.11, 22.12
+ });
+// ----------------------------------------------------------------
+    nd4j::ops::roll<double> op;
+
+    auto result = op.execute({&x}, {}, {6});
+    ASSERT_EQ(result->status(), Status::OK());
+
+//    x.printIndexedBuffer("images");
+//    nd4j_printf("input params: ksize = [1, 2, 1, 1], strides = [1, 1, 1, 1], rates = [1, 1, 1, 1]\n", "");
+//    result->at(0)->printIndexedBuffer("Output");
+    //result->at(0)->printShapeInfo("Out Shape");
+//    exp.printIndexedBuffer("Expect");
+    //exp.printShapeInfo("Exp Shape");
+//    ASSERT_TRUE(exp.isSameShape(result->at(0)));
+    ASSERT_TRUE(exp.equalsTo(result->at(0)));
+
+    delete result;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+TEST_F(DeclarableOpsTests7, TestRoll_2) {
+    NDArray<double> x('c', {2, 2, 4, 2}, {
+    11.11, 11.12, 11.21, 11.22, 11.31, 11.32, 11.41, 11.42,     12.11, 12.12, 12.21, 12.22, 12.31, 12.32, 12.41, 12.42,
+    21.11, 21.12, 21.21, 21.22, 21.31, 21.32, 21.41, 21.42,     22.11, 22.12, 22.21, 22.22, 22.31, 22.32, 22.41, 22.42
+});
+
+NDArray<double> exp('c', {2, 2, 4, 2}, {
+         12.11, 12.12, 12.21, 12.22, 12.31, 12.32, 12.41, 12.42,    21.11, 21.12, 21.21, 21.22, 21.31, 21.32, 21.41, 21.42,
+         22.11, 22.12, 22.21, 22.22, 22.31, 22.32, 22.41, 22.42,    11.11, 11.12, 11.21, 11.22, 11.31, 11.32, 11.41, 11.42
+ });
+// ----------------------------------------------------------------
+    nd4j::ops::roll<double> op;
+
+    auto result = op.execute({&x}, {}, {-8});
+    ASSERT_EQ(result->status(), Status::OK());
+
+//    x.printIndexedBuffer("images");
+//    nd4j_printf("input params: ksize = [1, 2, 1, 1], strides = [1, 1, 1, 1], rates = [1, 1, 1, 1]\n", "");
+    result->at(0)->printIndexedBuffer("Output");
+    //result->at(0)->printShapeInfo("Out Shape");
+    exp.printIndexedBuffer("Expect");
+    //exp.printShapeInfo("Exp Shape");
+//    ASSERT_TRUE(exp.isSameShape(result->at(0)));
+    ASSERT_TRUE(exp.equalsTo(result->at(0)));
+
+    delete result;
+}
+
+
 
 
 
